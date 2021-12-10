b9b0a150a322f27a2cf4cbff635e6f9cee1654bb
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-10 19:50:25 +0000

    Update ping.js

--- src/commands/Common/ping.js
+++ src/commands/Common/ping.js
@@ -1,6 +1,7 @@
 module.exports = ({
     name: "ping",
-    code: `My ping is \`$ping ms\`
+    code: `**Bot Ping**: \`$ping ms\`
+**Database Ping:** \`$dbPing ms\`
     $globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
 		$suppressErrors`,
 })
