ab20ed45d68fe41bc7ada4a37b45b3aaca8057ec
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-13 15:08:21 +0000

    Fix missing args

--- src/commands/Interactions/8ball.js
+++ src/commands/Interactions/8ball.js
@@ -1,10 +1,15 @@
 module.exports = {
     name : "8ball",
-    code: `$title[You ask the magic 8ball...]
+    code: `
+    $if[$message==]
+    Usage: $getServerVar[Prefix]8ball <question>
+    $else
+    $title[You ask the magic 8ball...]
     $description[$addField[Answer:;$httpRequest[https://apiv1.snoozeds.repl.co/8ball/$message;GET;;response;Failed to grab answer.]]$addField[Question:;$message]]
     $thumbnail[https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/mozilla/36/billiards_1f3b1.png]
     $addTimestamp
     $color[$getGlobalUserVar[EmbedColor;$authorID]]
-    $globalCooldown[10s;Please wait %time%.]
+    $endif
+    $globalCooldown[10s;Please wait **%time%**.]
     `
 }
