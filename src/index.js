4874d52b1c8963b219ee18fd004f8858d9b52106
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-14 13:16:52 +0000

    Update crafting variables

--- src/index.js
+++ src/index.js
@@ -17,22 +17,27 @@ bot.variables({
   Coins: "0",
   HP: "100",
   MaxHP : "100",
-  ATK: "1",
+	ATK: "1",
   Prefix: "d!",
   EmbedColor : "#FFE302",
   FamilyMode : "False",
   UserInt : "True",
-  FirstDaily: "True",
-  DailyAchievement: "<:Locked:899050875916541963>",
-  FarmO: "<:Orange:861639407978020914>",
-  FarmK: "<:Kiwi:861639983268626442>",
-  FarmS: "<:Strawberry:861641050930282516>",
-  FirstFarm: "True",
-  FarmAchievement: "<:Locked:899050875916541963>",
-  FirstATK: "True",
-  ATKAchievement: "<:Locked:899050875916541963>",
+	FirstDaily: "True",
+	DailyAchievement: "<:Locked:899050875916541963>",
+	FarmO: "<:Orange:861639407978020914>",
+	FarmK: "<:Kiwi:861639983268626442>",
+	FarmS: "<:Strawberry:861641050930282516>",
+	FirstFarm: "True",
+	FarmAchievement: "<:Locked:899050875916541963>",
+	FirstATK: "True",
+	ATKAchievement: "<:Locked:899050875916541963>",
   InBusiness: "False",
   BusinessType: "None",
+  //////////////////////////////////////////////////
+  Stone : "0",
+  Wood : "0",
+  Slime: "0",
+  //////////////////////////////////////////////////
   }) 
   // These are the DEFAULTS for each variable.
 
@@ -43,6 +48,18 @@ bot.loadCommands(`./commands/Economy`)
 bot.loadCommands(`./commands/Shop`)
 bot.loadCommands(`./commands/Interactions`)
 bot.loadCommands(`./commands/Experimental`)
+bot.loadCommands(`./commands/Stats`)
+
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Running discord bot....!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
+
 
 // Error handler so bot doesn't kaboom
 try{}catch(error){console.log(error)}
