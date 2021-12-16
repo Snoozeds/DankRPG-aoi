27ae090b16fdcc3d8518b4a1d453fff98654b45e
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-16 00:09:12 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,23 +14,24 @@ bot.status({
     })
 
 bot.variables({
+  Coi: "<:RPGCoin:855767372534906920>",
   Coins: "0",
   HP: "100",
   MaxHP : "100",
-  ATK: "1",
+	ATK: "1",
   Prefix: "d!",
   EmbedColor : "#FFE302",
   FamilyMode : "False",
   UserInt : "True",
-  FirstDaily: "True",
-  DailyAchievement: "<:Locked:899050875916541963>",
-  FarmO: "<:Orange:861639407978020914>",
-  FarmK: "<:Kiwi:861639983268626442>",
-  FarmS: "<:Strawberry:861641050930282516>",
-  FirstFarm: "True",
-  FarmAchievement: "<:Locked:899050875916541963>",
-  FirstATK: "True",
-  ATKAchievement: "<:Locked:899050875916541963>",
+	FirstDaily: "True",
+	DailyAchievement: "<:Locked:899050875916541963>",
+	FarmO: "<:Orange:861639407978020914>",
+	FarmK: "<:Kiwi:861639983268626442>",
+	FarmS: "<:Strawberry:861641050930282516>",
+	FirstFarm: "True",
+	FarmAchievement: "<:Locked:899050875916541963>",
+	FirstATK: "True",
+	ATKAchievement: "<:Locked:899050875916541963>",
   InBusiness: "False",
   BusinessType: "None",
   //////////////////////////////////////////////////
@@ -49,17 +50,6 @@ bot.loadCommands(`./commands/Shop`)
 bot.loadCommands(`./commands/Interactions`)
 bot.loadCommands(`./commands/Experimental`)
 bot.loadCommands(`./commands/Stats`)
-
-const http = require('http');
-
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Running discord bot....!');
-}
-
-const server = http.createServer(requestListener);
-server.listen(8080);
-
 
 // Error handler so bot doesn't kaboom
 try{}catch(error){console.log(error)}
