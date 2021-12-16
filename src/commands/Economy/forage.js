2f91e16fb1b80cc855bfe770197a1db8c594794d
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-16 00:08:38 +0000

    Section code in forage.js better

--- src/commands/Economy/forage.js
+++ src/commands/Economy/forage.js
@@ -1,30 +1,36 @@
 module.exports = {
     name: "forage",
-    aliases: ["explore", "search"],
+    aliases: ["explore", "search", "forage"],
     code: `
+    
     $if[$randomText[1;1;1;2;2;2;3;3;3;4]==1]
     $description[<@$authorID> goes foraging\nThey find Wood **($random[1;5])**]
-    $color[$getGlobalUserVar[EmbedColor]]
     $setGlobalUserVar[Wood;$sum[$getGlobalUserVar[Wood;$authorID];$random[1;5]];$authorID]
-    
-    $else
-    $if[$randomText[1;1;1;2;2;2;3;3;3;4]==2]
-    $description[<@$authorID> goes foraging\nThey find Stone **($random[1;3])**]
     $color[$getGlobalUserVar[EmbedColor]]
-    $setGlobalUserVar[Stone;$sum[$getGlobalUserVar[Stone;$authorID];$random[1;3]];$authorID]
     $else
 
-$if[$randomText[1;1;1;2;2;2;3;3;3;4]==3]
-    $description[<@$authorID> goes foraging\nThey find Slime **($random[1;2])**]
+    $if[$randomText[1;1;1;2;2;2;3;3;3;4]==2]
+    $description[<@$authorID> goes foraging\nThey find Stone **($random[1;3])**]
+    $setGlobalUserVar[Stone;$sum[$getGlobalUserVar[Stone;$authorID];$random[1;3]];$authorID]
     $color[$getGlobalUserVar[EmbedColor]]
-    $setGlobalUserVar[Slime;$sum[$getGlobalUserVar[Slime;$authorID];$random[1;2]];$authorID]
     $else
+
+    $if[$randomText[1;1;1;2;2;2;3;3;3;4]==3]
     $description[<@$authorID> goes foraging\nThey find Slime **($random[1;2])**]
-    $color[$getGlobalUserVar[EmbedColor]]
     $setGlobalUserVar[Slime;$sum[$getGlobalUserVar[Slime;$authorID];$random[1;2]];$authorID]
+    $color[$getGlobalUserVar[EmbedColor]]
+    $else
+
+    $if[$randomText[1;1;1;2;2;2;3;3;3;4]==4]
+    $description[<@$authorID> goes foraging\nThey find Slime **($random[1;2])**]
+    $setGlobalUserVar[Slime;$sum[$getGlobalUserVar[Slime;$authorID];$random[1;2]];$authorID]
+    $color[$getGlobalUserVar[EmbedColor]]
+    
     $endif
     $endif
     $endif
+    $endif
+
     $globalCooldown[10s;Calm down! There's nothing to forage. (**%time%**)]
     `
-} // For some reason it only works with slime being there twice. Sadge.
+}
