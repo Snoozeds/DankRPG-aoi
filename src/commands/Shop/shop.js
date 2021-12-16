1caca7ac56f307073f4fe42d462796c978f94e38
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-16 19:34:21 +0000

    Update shop.js

--- src/commands/Shop/shop.js
+++ src/commands/Shop/shop.js
@@ -3,7 +3,8 @@ module.exports = {
 	code: `
 	$title[Welcome to my shop!]
 	$description[Here are my items on sale.\n\n<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your HP by 100. | <:RPGCoin:855767372534906920>**750**\n$getServerVar[Prefix]buyhp\n\n
-	]
+	<:ItmHouse:921118526876577814> **__House__**\nAutomatically heals HP as you type. | **500 Wood**\n$getServerVar[Prefix]buyhouse\n\n
+Other Shop commands:\n\`upgrade\`, \`heal\`]
 	$color[$getGlobalUserVar[EmbedColor]]
 	$suppressErrors
 	`
