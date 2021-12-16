98cccaa617f2e84da584a69085f7d7b12c1e6df7
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-16 19:35:11 +0000

    Cleaner embed (commands.js)

--- src/commands/Common/commands.js
+++ src/commands/Common/commands.js
@@ -2,44 +2,28 @@ module.exports = {
     name: "commands",
     aliases: ["help", "cmds"],
     code: `
-		$if[$checkContains[$userPerms;Manage Guild]==true]
-		$title[Commands: Guild Manager]
-		$color[$getGlobalUserVar[EmbedColor]]
-		 $description[**__Misc__**
-\`commands\`, \`ping\`, \`settings\`, \`feedback\`, \`credits\`, \`repeat\`, \`8ball\`\n
-**__Economy__**
-\`daily\`, \`fight\`, \`shop\`, \`farm\`, \`bs\`, \`bwork\`, \`forage\`\n
-**__Stats__**
-\`info\`, \`profile\`, \`hp\`, \`achievements\`, \`whois\`, \`serverinfo\`, \`inventory\`\n
-**__Interactions__**
-\`hug\`, \`kill\`\n
-**__Experimental Features__**
-**Music:** \`play\`, \`skip\`, \`stop\`, \`cp\`, \`loopsong\`\n
-**__Server Settings__**
-\`prefix\`\n\n
+$title[Commands]
+$color[$getGlobalUserVar[EmbedColor]]
+$description[
+**Economy Commands**:
+\`daily, fight, farm, bwork, forage\`
+
+**Interactions Commands**:
+\`hug, kill\`
+
+**Fun Commands**:
+\`repeat, 8ball\`
+
+**Misc Commands**:
+\`commands, ping, info, feedback, credits\`
+
+**Settings**:
+\`prefix, changecolor, familymode, interactions\`
+
 **__Bot Links__**
 [Privacy](https://dankrpg.xyz/privacy) | [Support](https://discord.gg/Cc3xBSpWeB) | [Docs](https://docs.dankrpg.xyz) | [Source Code](https://github.com/SnoozedsGithub/DankRPG)]
-		$footer[You are seeing this menu because you have the "manage guild" permission.]
-
-		$else
-		$title[Commands]
-    $color[$getGlobalUserVar[EmbedColor]]
-    $description[**__Misc__**
-\`commands\`, \`ping\`, \`settings\`, \`feedback\`, \`credits\`, \`repeat\`, \`8ball\`\n
-**__Economy__**
-\`daily\`, \`fight\`, \`shop\`, \`farm\`, \`bs\`, \`bwork\`, \`forage\`\n
-**__Stats__**
-\`info\`, \`profile\`, \`hp\`, \`achievements\`, \`whois\`, \`serverinfo\`, \`inventory\`\n
-**__Interactions__**
-\`hug\`, \`kill\`\n
-**__Experimental Features__**
-**Music:** \`play\`, \`skip\`, \`stop\`, \`cp\`, \`loopsong\`\n\n
-**__Bot Links__**
-[Privacy](https://dankrpg.xyz/privacy) | [Support](https://discord.gg/Cc3xBSpWeB) | [Docs](https://docs.dankrpg.xyz) | [Source Code](https://github.com/SnoozedsGithub/DankRPG)]
-
-		$endif
-		$suppressErrors
-		$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
+$suppressErrors
+$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
     
 
 
