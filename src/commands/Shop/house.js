31bf8543a1ae56b2ba6ca01504a32ab168e026f5
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-16 19:34:03 +0000

    Create house.js

---
+++ src/commands/Shop/house.js
@@ -0,0 +1,22 @@
+module.exports = [{
+    name: "buyhouse",
+    aliases: "buildhouse",
+    code: `
+    You bought a house for **500 Wood**
+    $setGlobalUserVar[Wood;$sub[$getGlobalUserVar[Wood];500];$authorID]
+    $setGlobalUserVar[BoughtHouse;True]
+    $onlyIf[$getGlobalUserVar[Wood;$authorID]>499;You do not have enough **wood** to buy a **house**. You need at least **499 Wood**.]
+    $onlyIf[$getGlobalUserVar[BoughtHouse]!=True;You already have a house smh]`
+},
+
+{
+    name: "$alwaysExecute",
+    code: `
+    $setGlobalUserVar[HP;$sum[$getGlobalUserVar[HP];1]]
+    $onlyIf[$getGlobalUserVar[BoughtHouse]==True;]
+    $onlyIf[$getGlobalUserVar[HP]!=$sub[$getGlobalUserVar[MaxHP];1];]
+    $onlyIf[$getGlobalUserVar[HP]!=$getGlobalUserVar[MaxHP];]
+    $globalCooldown[60s;]`
+}
+
+]
