fdbf7d825d6ec01a1b2c6920e70fd32e9e291375
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-17 09:52:27 +0000

    Update shop.js

--- src/commands/Shop/shop.js
+++ src/commands/Shop/shop.js
@@ -2,8 +2,9 @@ module.exports = {
 	name: "shop",
 	code: `
 	$title[Welcome to my shop!]
-	$description[Here are my items on sale.\n\n<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your HP by 100. | <:RPGCoin:855767372534906920>**750**\n$getServerVar[Prefix]buyhp\n\n
-	<:ItmHouse:921118526876577814> **__House__**\nAutomatically heals HP as you type. | **500 Wood**\n$getServerVar[Prefix]buyhouse\n\n
+	$description[Here are my items on sale.\n\n<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your HP by 100. | <:RPGCoin:855767372534906920>**750**\n**In stock: ∞**\n$getServerVar[Prefix]buyhp\n\n
+	<:ItmHouse:921118526876577814> **__House__**\nAutomatically heals HP as you type. | **500 Wood**\n**In stock: $getGlobalUserVar[HouseL;$authorID]**\n$getServerVar[Prefix]buyhouse\n\n
+	:gift: **__Present__**\nContains a suprise! | <:RPGCoin:855767372534906920>**100**\n**In stock: $getGlobalUserVar[GiftL;$authorID]**\n$getServerVar[Prefix]buypresent\n\n
 Other Shop commands:\n\`upgrade\`, \`heal\`]
 	$color[$getGlobalUserVar[EmbedColor]]
 	$suppressErrors
