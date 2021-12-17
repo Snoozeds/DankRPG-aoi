974d37266027df29003ebf3813b521163a3bc492
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-17 09:51:23 +0000

    Update house.js

--- src/commands/Shop/house.js
+++ src/commands/Shop/house.js
@@ -5,6 +5,7 @@ module.exports = [{
     You bought a house for **500 Wood**
     $setGlobalUserVar[Wood;$sub[$getGlobalUserVar[Wood];500];$authorID]
     $setGlobalUserVar[BoughtHouse;True]
+    $setGlobalUserVar[HouseL;0]
     $onlyIf[$getGlobalUserVar[Wood;$authorID]>499;You do not have enough **wood** to buy a **house**. You need at least **499 Wood**.]
     $onlyIf[$getGlobalUserVar[BoughtHouse]!=True;You already have a house smh]`
 },
