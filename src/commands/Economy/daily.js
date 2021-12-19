b291fef99e784074e087fc3685a5c598f6a69def
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-19 13:55:25 +0000

    Update daily.js

--- src/commands/Economy/daily.js
+++ src/commands/Economy/daily.js
@@ -3,13 +3,13 @@ module.exports = {
     code: `
 		$if[$getGlobalUserVar[FirstDaily;$authorID]==False]
 		<:Unlocked:899050875719393281> You collected your daily reward of <:RPGCoin:855767372534906920>100.
-    $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];100]]
+    $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];250]]
 		$setGlobalUserVar[FirstDaily;False]
 		$else
 			<:Unlocked:899050875719393281> You collected your daily reward of <:RPGCoin:855767372534906920>100.
 **Congratulations!** You unlocked an achievement: "Your first time"\n+<:RPGCoin:855767372534906920>**100**.
 		$setGlobalUserVar[DailyAchievement;<:Unlocked:899050875719393281>]
-    $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];200]]
+    $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];350]]
 		$setGlobalUserVar[FirstDaily;False]
 		$endif
 		$globalCooldown[24h;You have already collected your daily reward today. Please come back in **%time%**.]
