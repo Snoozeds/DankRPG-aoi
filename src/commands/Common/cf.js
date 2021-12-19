10e77b0e12f5fa79335d876aed181ae7159d28c3
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-19 20:06:35 +0000

    New coinflip command

---
+++ src/commands/Common/cf.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "coinflip",
+    aliases: ["cf", "fc", "flip", "flipcoin"],
+    code: `
+    Flipping a coin $editIn[350ms;Flipping a coin.;Flipping a coin..;Flipping a coin...;It landed on **$randomText[heads;tails]**!]
+    $globalCooldown[10s;Please wait %time%.]`
+}
