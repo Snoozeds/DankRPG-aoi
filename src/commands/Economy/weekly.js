391fff0fcf32b0e93e370cc2ba79dbc08d91f313
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-26 17:33:18 +0000

    Fix cooldown (weekly.js)

--- src/commands/Economy/weekly.js
+++ src/commands/Economy/weekly.js
@@ -4,7 +4,6 @@ module.exports = {
     $title[Unfortunately, you haven't voted.]
     $description[<:tggfail:924458621440495658> Using the 'weekly' command requires you to vote for us on top.gg.
 This only takes a couple of seconds and helps support us. [vote](https://top.gg/bot/855479925863481345/vote)
-
 **Your rewards for voting:**
 +$getVar[Coi]**1000**]
     $footer[Our privacy policy applies to this command: https://dankrpg.xyz/privacy]
@@ -14,6 +13,6 @@ This only takes a couple of seconds and helps support us. [vote](https://top.gg/
     Thank you for voting! Your support helps the bot grow :)
 +$getVar[Coi]**1000**
 $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];1000];$authorID]
-$globalCooldown[12h;You can only claim your voting rewards every 12 hours. You need to wait **%time%**.]
+$globalCooldown[7d;You can only claim your voting rewards every week. You need to wait **%time%**.]
     $endif`
 }
