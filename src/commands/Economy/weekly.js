eeb7c605959b642940e93919884ebffe04c78275
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2021-12-26 01:28:07 +0000

    Fix cooldown (weekly.js)

--- src/commands/Economy/weekly.js
+++ src/commands/Economy/weekly.js
@@ -9,11 +9,11 @@ This only takes a couple of seconds and helps support us. [vote](https://top.gg/
 +$getVar[Coi]**1000**]
     $footer[Our privacy policy applies to this command: https://dankrpg.xyz/privacy]
     $color[$getGlobalUserVar[EmbedColor;$authorID]]
-    $cooldown[15s;Slow down! You still haven't voted. **%time%** left.]
+    $globalCooldown[15s;Slow down! You still haven't voted. **%time%** left.]
     $else
     Thank you for voting! Your support helps the bot grow :)
 +$getVar[Coi]**1000**
 $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];1000];$authorID]
-$cooldown[12h;You can only claim your voting rewards every 12 hours. You need to wait **%time%**.]
+$globalCooldown[12h;You can only claim your voting rewards every 12 hours. You need to wait **%time%**.]
     $endif`
 }
