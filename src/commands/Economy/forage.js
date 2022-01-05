328edf3ce6b6a056be64d4357aac3bd7b5a5a124
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-01-05 10:09:15 +0000

    It finally works! (forage.js)

--- src/commands/Economy/forage.js
+++ src/commands/Economy/forage.js
@@ -1,28 +1,9 @@
 module.exports = {
     name: "forage",
     aliases: ["explore", "search"],
-    code: `$if[$randomText[1;2;3]==1]
-    $description[<@$authorID> goes foraging\nThey find Wood **($random[1;5])**]
-    $setGlobalUserVar[Wood;$sum[$getGlobalUserVar[Wood;$authorID];$random[1;5]];$authorID]
-    $color[$getGlobalUserVar[EmbedColor]]
-    $else
-
-    $if[$randomText[1;2;3]==2]
-    $description[<@$authorID> goes foraging\nThey find Stone **($random[1;3])**]
-    $setGlobalUserVar[Stone;$sum[$getGlobalUserVar[Stone;$authorID];$random[1;3]];$authorID]
-    $color[$getGlobalUserVar[EmbedColor]]
-    $else
-
-    $if[$randomText[1;2;3]==3]
-    $description[<@$authorID> goes foraging\nThey find Slime **($random[1;2])**]
-    $setGlobalUserVar[Slime;$sum[$getGlobalUserVar[Slime;$authorID];$random[1;2]];$authorID]
-    $color[$getGlobalUserVar[EmbedColor]]
-
-    $endif
-    $endif
-    $endif
-
+    code: `$description[<@$authorID> goes foraging. They find: $randomText[Wood;Stone;Slime] (**$random[1;3]**)]
+    $setGlobalUserVar[$randomText[Wood;Stone;Slime];$sum[$getGlobalUserVar[$randomText[Wood;Stone;Slime]];$random[1;3]];$authorID]
+    $color[$getGlobalUserVar[EmbedColor;$authorID]]
     $globalCooldown[10s;Please wait **%time%**]
-    
     `
 }
