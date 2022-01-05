3fd558269f55b15cd76cc7beed10980ad1dc80cd
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-01-05 10:20:21 +0000

    Credit pets (credits.js)

--- src/commands/Common/credits.js
+++ src/commands/Common/credits.js
@@ -1,7 +1,8 @@
 module.exports = {
 	name: "credits",
 	code: `$title[Credits]
-	$description[\`Farm\`:\n"[LPC] Crops" by bluecarrot16, Daniel Eddeland (daneeklu), Joshua Taylor, Richard Kettering (Jetrel). Commissioned by castelonia. \n[CC-BY-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) | [Link](https://opengameart.org/content/lpc-crops)]
+	$description[\`Farm\`:\n"[LPC] Crops" by bluecarrot16, Daniel Eddeland (daneeklu), Joshua Taylor, Richard Kettering (Jetrel). Commissioned by castelonia. \n[CC-BY-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) | [Link](https://opengameart.org/content/lpc-crops)
+\`Petinfo, buypet\`"\n"googlefonts/noto-emoji"\n[OFL-1.1](https://opensource.org/licenses/OFL-1.1)]
 	$color[$getGlobalUserVar[EmbedColor]]
 	$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
 	$suppressErrors
