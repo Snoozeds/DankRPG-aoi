f41e8c3d500d789817e82667d57e88aba72e4082
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-01-16 11:50:05 +0000

    Add mining variables (inventory.js)

--- src/commands/Stats/inventory.js
+++ src/commands/Stats/inventory.js
@@ -1,17 +1,22 @@
 module.exports = {
-    name: "inventory",
-    aliases: ["inv", "i"],
-    code: `$title[$username[$findUser[$message;yes]]'s Inventory]
+name: "inventory",
+aliases: ["inv", "i"],
+code: `$title[$username[$findUser[$message;yes]]'s Inventory]
 $description[
 $if[$getGlobalUserVar[Wood;$findUser[$message;yes]]!=0]
 **Wood:** $getGlobalUserVar[Wood;$findUser[$message;yes]]
 $endif
-$if[$getGlobalUserVar[Stone;$findUser[$message;yes]]!=0]
-**Stone:** $getGlobalUserVar[Stone;$findUser[$message;yes]]
-$endif
 $if[$getGlobalUserVar[Slime;$findUser[$message;yes]]!=0]
 **Slime:** $getGlobalUserVar[Slime;$findUser[$message;yes]]
 $endif
+$if[$getGlobalUserVar[Stone;$findUser[$message;yes]]!=0]
+**Stone:** $getGlobalUserVar[Stone;$findUser[$message;yes]]
+$endif
+$if[$getGlobalUserVar[Gold;$findUser[$message;yes]]!=0]
+**Gold:** $getGlobalUserVar[Gold;$findUser[$message;yes]]
+$endif
+$if[$getGlobalUserVar[Diamond;$findUser;$message;yes]!=0]
+**Diamond:** $getGlobalUserVar[Diamond;$findUser[$messsage;yes]]
 ]
 $color[$getGlobalUserVar[EmbedColor]]
 $globalCooldown[5s;Please wait **%time%**.]`
