f287440d5efbfd2e4ffc0e3710ec31571e9930c5
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-01-27 14:41:24 +0000

    Update voting api.

--- src/commands/Economy/weekly.js
+++ src/commands/Economy/weekly.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "weekly",
-    code: `$if[$checkContains[$httpRequest[https://normal-api.ml/topgg/hasvoted?bot=$clientID&user=$authorID&token=$getVar[TopggToken]];true]!=true]
+    code: `
+    $if[$httpRequest[https://top.gg/api/bots/855479925863481345/check?userId=$authorID;GET;;voted;;Authorization:$getVar[TopggToken]]==0]
     $title[Unfortunately, you haven't voted.]
     $description[<:Locked:899050875916541963> Using the 'weekly' command requires you to vote for us on top.gg.
 This only takes a couple of seconds and helps support us. [vote](https://top.gg/bot/855479925863481345/vote)
@@ -8,6 +9,7 @@ This only takes a couple of seconds and helps support us. [vote](https://top.gg/
 +$getVar[Coi]**1000**]
     $footer[Our privacy policy applies to this command: https://dankrpg.xyz/privacy]
     $color[$getGlobalUserVar[EmbedColor;$authorID]]
+    $globalCooldown[1m;You haven't voted. Please try again.]
     $else
     Thank you for voting! Your support helps the bot grow :)
 +$getVar[Coi]**1000**
