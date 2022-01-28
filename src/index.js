04bbb755bd3030eaf21f95072d4efb3c387e8226
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-01-28 14:02:41 +0000

    + add marriage variables.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 const { AutoPoster } = require('topgg-autoposter')
 
-const ap = AutoPoster('', client)
+const ap = AutoPoster(config.topgg, client)
 
 ap.on('posted', () => {
   console.log('Posted stats to Top.gg!')
@@ -32,45 +32,45 @@ bot.variables({
   Coins: "0",
   HP: "100",
   MaxHP : "100",
-  ATK: "1",
+	ATK: "1",
   Prefix: "d!",
   EmbedColor : "#FFE302",
   FamilyMode : "False",
   UserInt : "True",
-  FirstDaily: "True",
-  DailyAchievement: "<:Locked:899050875916541963>",
-  FarmO: "<:Orange:861639407978020914>",
-  FarmK: "<:Kiwi:861639983268626442>",
-  FarmS: "<:Strawberry:861641050930282516>",
-  FirstFarm: "True",
-  FarmAchievement: "<:Locked:899050875916541963>",
-  FirstATK: "True",
-  ATKAchievement: "<:Locked:899050875916541963>",
+	FirstDaily: "True",
+	DailyAchievement: "<:Locked:899050875916541963>",
+	FarmO: "<:Orange:861639407978020914>",
+	FarmK: "<:Kiwi:861639983268626442>",
+	FarmS: "<:Strawberry:861641050930282516>",
+	FirstFarm: "True",
+	FarmAchievement: "<:Locked:899050875916541963>",
+	FirstATK: "True",
+	ATKAchievement: "<:Locked:899050875916541963>",
   InBusiness: "False",
   BusinessType: "None",
-  //
   Stone : "0",
   Wood : "0",
   Slime: "0",
-  //
   BoughtHouse: "False", // Bought house or not
   GiftL: "1", // Gift Stock
   HouseL: "1", // House Stock
   TopggToken: "",
-  //
   PetType: "none",
   PetName: "Your pet hasn't been named yet. Run \`<prefix>petname <name>\`.",
   HasPet: "false",
   PetIMG : "",
-  //
   Gold: "0",
   Diamond: "0",
   SlimeEmoji: "<:Slime:932047284814884996>",
   StoneEmoji: "<:Stone:932047285058166814>",
   GoldEmoji: "<:Gold:932047285213360208>",
   DiamondEmoji: "<:Diamond:932047284986839110>",
-  //
-
+  AcceptEmoji: "<:Unlocked:899050875719393281>",
+  DenyEmoji: "<:Locked:899050875916541963>",
+  Sender: "",
+  MarriageDate: "", // Time in unix since user was married.
+  InRelationship: "False",
+  MarriedTo: "",
   // These are the DEFAULTS for each variable.
   })
 bot.onMessage()
@@ -83,7 +83,6 @@ bot.loadCommands(`./commands/Stats`)
 
 const http = require('http');
 
-// Runs a page on port 8080, you can ping this to keep it alive; or you could monitor it.
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end('Running discord bot....!');
