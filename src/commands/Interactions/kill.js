81791c4796c7c58ff52b82a943324b50c17a8a76
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-01-29 18:14:41 +0000

    Fix familyMode + remove bot kill restriction.

--- src/commands/Interactions/kill.js
+++ src/commands/Interactions/kill.js
@@ -1,13 +1,11 @@
 module.exports = {
-	name: "kill",
-	code: `
-	$globalCooldown[10s;Please wait **%time%** before using this userInteraction command again.]
-	$onlyif[$getGlobalUserVar[FamilyMode;$mentioned[1]]!=True;]
-	$onlyif[$getGlobalUserVar[FamilyMode;$authorID!=True;]
-	$onlyif[$isbot[$mentioned[1]]!=true;BOTS WILL NEVER DIE]
-	$onlyif[$mentioned[1]!=$authorID;I can't allow you to do that to yourself.]
-	$onlyif[$getGlobalUserVar[UserInt;$mentioned[1]]!=False;This user has interactions turned off.]
-	$username kills $username[$mentioned[1]] by $randomText[Yeeting them;defenestrating them;ejecting them;stabbing them;kicking them;bonking them to death]
-	$suppressErrors
+name: "kill",
+code: `
+$username kills $username[$findUser[$message;no]] by $randomText[Yeeting them;defenestrating them;ejecting them;stabbing them;kicking them;bonking them to death]
+$onlyIf[$getGlobalUserVar[FamilyMode;$findUser[$message;no]]!=True;User has family mode.]
+$onlyIf[$findUser[$message;no]!=$clientID;haha I am immortal\nL]
+$onlyIf[$findUser[$message;no]!=$authorID;I can't allow you to do that to yourself.]
+$onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has interactions turned off.]
+$globalCooldown[10s;Please wait **%time%** before using this userInteraction command again.]
 	`
 }
