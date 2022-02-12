1ebb09594ecdcfd35bcfe5a53de3e9acc39b575e
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-12 21:20:34 +0000

    Update avatar.js

--- src/commands/Interactions/avatar.js
+++ src/commands/Interactions/avatar.js
@@ -9,6 +9,6 @@ $image[$userAvatar[$mentioned[1;yes]]]
 $description[Download: [Link]($userAvatar)]
 $footer[Requested by $username[$authorID]]
 $addTimestamp
-$onlyIf[$getGlobalUserVar[UserInt;$mentioned[1]]!=False;This user has interaction commands disabled from their settings.]
+$onlyIf[$getGlobalUserVar[UserInt;$mentioned[1]]!=False;This user has user interactions disabled from their settings.]
 $suppressErrors[Mention someone.]`
 }
