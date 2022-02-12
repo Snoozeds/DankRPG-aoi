b25b142f11259a970905fdb8acdc52acab0389e2
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-12 21:19:48 +0000

    Change 'userInteraction' to 'social' (kill.js)

--- src/commands/Interactions/kill.js
+++ src/commands/Interactions/kill.js
@@ -6,6 +6,6 @@ $onlyIf[$getGlobalUserVar[FamilyMode;$findUser[$message;no]]!=True;User has fami
 $onlyIf[$findUser[$message;no]!=$clientID;haha I am immortal\nL]
 $onlyIf[$findUser[$message;no]!=$authorID;I can't allow you to do that to yourself.]
 $onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has interactions turned off.]
-$globalCooldown[10s;Please wait **%time%** before using this userInteraction command again.]
+$globalCooldown[10s;Please wait **%time%** before using this social command again.]
 	`
 }
