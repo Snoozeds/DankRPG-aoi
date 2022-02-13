3874a912586363771bb0cbf65fc98a2645e0d484
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-13 17:59:27 +0000

    Misc. Changes to dungeon.js

--- src/commands/Economy/dungeon.js
+++ src/commands/Economy/dungeon.js
@@ -1,11 +1,15 @@
 module.exports = {
-name: "startdungeon",
+name: "dungeon",
+aliases: "startdungeon",
 code: `You start a dungeon.
-$if[$randomText[Loot;Enemy;Enemy]==Enemy]
+$if[$randomText[Loot;Enemy]==Enemy]
 You find an enemy. It damages you, and you lose $getVar[Heart]$random[5;10].
 $setGlobalUserVar[HP;$sub[$getGlobalUserVar[HP;$authorID];$random[5;10]];$authorID]
 $else
-You find $getVar[Coi]$random[25;50].
+$title[Random Event!]
+$color[$getGlobalUserVar[EmbedColor;$authorID]]
+$description[You find $getVar[Coi]$random[25;50].]
+$thumbnail[https://assets.dankrpg.xyz/Images/chest.png]
 $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$authorID];$random[25;50]];$authorID]
 $endif
 $globalCooldown[30s;Please wait **%time%** before starting another dungeon.]`
