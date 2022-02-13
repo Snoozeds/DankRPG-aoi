e662d7a6f315700c5cb5378fdff2e20cfd545c92
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-13 17:40:29 +0000

    Update embed in info.js

--- src/commands/Common/info.js
+++ src/commands/Common/info.js
@@ -3,12 +3,17 @@ name: "info",
 code: `
 $description[
 **__Bot Info__**
-Ping: $round[$ping]ms
+Bot Ping: $round[$ping]ms
+Database Ping: $round[$dbPing]ms
 Uptime: $uptime
-Server Count: $serverCount \n
+Server Count: $serverCount\n
 **__Server Info__**
+OS: Linux, Ubuntu (20.04.3)
 RAM Usage: $round[$ram]MBs
-CPU Usage: $round[$cpu]%
+CPU Usage: $round[$cpu]% \n
+**__Package Info__**
+Node: $nodeVersion
+Aoi.JS: v$packageVersion
 ]
 $color[$getGlobalUserVar[EmbedColor]]
 $globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
