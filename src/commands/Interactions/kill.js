4287428a7d069fc5ed59ebb503bb6c4ad4aad548
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-13 12:50:24 +0000

    Add suppressErrors to kill.js

--- src/commands/Interactions/kill.js
+++ src/commands/Interactions/kill.js
@@ -7,5 +7,6 @@ $onlyIf[$findUser[$message;no]!=$clientID;haha I am immortal\nL]
 $onlyIf[$findUser[$message;no]!=$authorID;I can't allow you to do that to yourself.]
 $onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has interactions turned off.]
 $globalCooldown[10s;Please wait **%time%** before using this social command again.]
+$suppressErrors[Please mention a user.]
 	`
 }
