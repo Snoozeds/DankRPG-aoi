94016a2c8d400de2994c78e681099b52335af486
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-13 13:17:19 +0000

    Cleaner embed in info.js

--- src/commands/Common/info.js
+++ src/commands/Common/info.js
@@ -1,7 +1,7 @@
 module.exports = ({
-    name: "info",
-    code: `
-    $description[
+name: "info",
+code: `
+$description[
 **__Bot Info__**
 Ping: $round[$ping]ms
 Uptime: $uptime
@@ -9,8 +9,8 @@ Server Count: $serverCount \n
 **__Server Info__**
 RAM Usage: $round[$ram]MBs
 CPU Usage: $round[$cpu]%
-    ]
-    $color[$getGlobalUserVar[EmbedColor]]
-    $globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
-		$suppressErrors`,
+]
+$color[$getGlobalUserVar[EmbedColor]]
+$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
+$suppressErrors`
 })
