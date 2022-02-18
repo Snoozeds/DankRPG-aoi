7b0e6b8412b5b9701a43ad965982dc1774ec9d8d
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-18 21:54:52 +0000

    Update info.js

--- src/commands/Common/info.js
+++ src/commands/Common/info.js
@@ -6,16 +6,18 @@ $description[
 Bot Ping: $round[$ping]ms
 Database Ping: $round[$dbPing]ms
 Uptime: $uptime
-Server Count: $serverCount\n
+$if[$authorID!=764564962815115267]
+Server Count: $serverCount
+$endif
 **__Server Info__**
 OS: Linux, Ubuntu (20.04.3)
 RAM Usage: $round[$ram]MBs
-CPU Usage: $round[$cpu]% \n
+CPU Usage: $round[$cpu]% \n 
 **__Package Info__**
 Node: $nodeVersion
-Aoi.JS: v$packageVersion
-]
+Aoi.JS: v$packageVersion]
 $color[$getGlobalUserVar[EmbedColor]]
 $globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
 $suppressErrors`
 })
+// You may be wondering what the if function in this is for. That is my discord ID; I don't want to obsess over numbers.
