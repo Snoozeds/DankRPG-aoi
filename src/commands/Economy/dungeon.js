0cd807101239c37b91b7358aed2f1c4be573d10e
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-22 11:37:46 +0000

    Add new dungeon event (will fix bug later today)

--- src/commands/Economy/dungeon.js
+++ src/commands/Economy/dungeon.js
@@ -1,16 +1,28 @@
-module.exports = {
+module.exports = [{
 name: "dungeon",
 aliases: "startdungeon",
-code: `You start a dungeon.
+code: `
+You start a dungeon.
 $if[$randomText[Loot;Enemy]==Enemy]
 You find an enemy. It damages you, and you lose $getVar[Heart]$random[5;10].
 $setGlobalUserVar[HP;$sub[$getGlobalUserVar[HP;$authorID];$random[5;10]];$authorID]
-$else
-$title[Random Event!]
-$color[$getGlobalUserVar[EmbedColor;$authorID]]
-$description[You find $getVar[Coi]$random[25;50].]
-$thumbnail[https://assets.dankrpg.xyz/Images/chest.png]
+$elseIf[$randomText[ChestEvent;MinigameEvent]==ChestEvent]
+You found a chest! It contains $getVar[Coi]**$random[25;50]**
 $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$authorID];$random[25;50]];$authorID]
+$endelseIf
+$elseIf[$randomText[ChestEvent;MinigameEvent]==MinigameEvent]
+**MINIGAME:** Type $randomText[Dank;Fight;Defend] in chat to earn your reward!
+$awaitMessages[$authorID;7s;$randomText[Dank;Fight;Defend];DungeonR;:x: Sorry, you didn't respond in time. You don't get anything.]
+$endelseIf
 $endif
-$globalCooldown[30s;Please wait **%time%** before starting another dungeon.]`
-}
+$globalCooldown[30s;Please wait **%time%** before starting another dungeon.]
+`
+},
+
+{
+name: "DungeonR",
+type: "awaitedCommand",
+code: `You responded in time! You earned <:RPGCoin:855767372534906920>**$random[50;60]**.
+$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];$random[50;60]];$authorID]
+`
+}]
