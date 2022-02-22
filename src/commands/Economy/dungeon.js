1f256b148f6684c6b351d77428a5ee14d852944a
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-22 11:48:25 +0000

    Increase HP loss (bug fixed itself..?)

--- src/commands/Economy/dungeon.js
+++ src/commands/Economy/dungeon.js
@@ -4,8 +4,8 @@ aliases: "startdungeon",
 code: `
 You start a dungeon.
 $if[$randomText[Loot;Enemy]==Enemy]
-You find an enemy. It damages you, and you lose $getVar[Heart]$random[5;10].
-$setGlobalUserVar[HP;$sub[$getGlobalUserVar[HP;$authorID];$random[5;10]];$authorID]
+You find an enemy. It damages you, and you lose $getVar[Heart]$random[10;20].
+$setGlobalUserVar[HP;$sub[$getGlobalUserVar[HP;$authorID];$random[10;20]];$authorID]
 $elseIf[$randomText[ChestEvent;MinigameEvent]==ChestEvent]
 You found a chest! It contains $getVar[Coi]**$random[25;50]**
 $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$authorID];$random[25;50]];$authorID]
