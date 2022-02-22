ccd1563f3e8d96ee5f3571bf73aa79f0c4c81f0b
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-22 11:01:53 +0000

    Change thumbnail url to own "cdn"

--- src/commands/Interactions/8ball.js
+++ src/commands/Interactions/8ball.js
@@ -6,7 +6,7 @@ module.exports = {
     $else
     $title[You ask the magic 8ball...]
     $description[$addField[Answer:;$httpRequest[https://apiv1.snoozeds.repl.co/8ball/$message;GET;;response;Failed to grab answer.]]$addField[Question:;$message]]
-    $thumbnail[https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/mozilla/36/billiards_1f3b1.png]
+    $thumbnail[https://assets.dankrpg.xyz/Images/8ball.png]
     $addTimestamp
     $color[$getGlobalUserVar[EmbedColor;$authorID]]
     $endif
