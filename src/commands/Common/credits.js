f5cd6be7bb06e54b256cda4385974bf219e7b5a1
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-22 12:04:57 +0000

    make credits + forage look cleaner

--- src/commands/Common/credits.js
+++ src/commands/Common/credits.js
@@ -1,10 +1,10 @@
 module.exports = {
-	name: "credits",
-	code: `$title[Credits]
-	$description[\`Farm\`:\n"[LPC] Crops" by bluecarrot16, Daniel Eddeland (daneeklu), Joshua Taylor, Richard Kettering (Jetrel). Commissioned by castelonia. \n[CC-BY-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) | [Link](https://opengameart.org/content/lpc-crops)
-\`Petinfo, buypet\`"\n"googlefonts/noto-emoji"\n[OFL-1.1](https://opensource.org/licenses/OFL-1.1)]
-	$color[$getGlobalUserVar[EmbedColor]]
-	$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
-	$suppressErrors
+name: "credits",
+code: `$title[Credits]
+$description[**Farm crops**:\n"[LPC] Crops" by bluecarrot16, Daniel Eddeland (daneeklu), Joshua Taylor, Richard Kettering (Jetrel).\nCommissioned by castelonia. \n[CC-BY-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) | [Link](https://opengameart.org/content/lpc-crops)
+\n**Pet thumbnails**:\n"googlefonts/noto-emoji"\n[OFL-1.1](https://opensource.org/licenses/OFL-1.1) | [Link](https://github.com/googlefonts/noto-emoji)]
+$color[$getGlobalUserVar[EmbedColor]]
+$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
+$suppressErrors
 	`
 }
