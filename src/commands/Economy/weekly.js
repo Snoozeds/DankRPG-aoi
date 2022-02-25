1f52fd8265e9ba48abaeda00989d0d4e7a1f5e2a
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-02-25 10:30:58 +0000

    Change url in weekly

--- src/commands/Economy/weekly.js
+++ src/commands/Economy/weekly.js
@@ -1,17 +1,17 @@
 module.exports = {
-    name: "weekly",
-    code: `
-    $if[$httpRequest[https://top.gg/api/bots/855479925863481345/check?userId=$authorID;GET;;voted;;Authorization:$getVar[TopggToken]]==0]
-    $title[Unfortunately, you haven't voted.]
-    $description[<:Locked:899050875916541963> Using the 'weekly' command requires you to vote for us on top.gg.
-This only takes a couple of seconds and helps support us. [vote](https://top.gg/bot/855479925863481345/vote)
+name: "weekly",
+code: `
+$if[$httpRequest[https://top.gg/api/bots/$clientID/check?userId=$authorID;GET;;voted;;Authorization:$getVar[TopggToken]]==0]
+$title[Unfortunately, you haven't voted.]
+$description[<:Locked:899050875916541963> Using the 'weekly' command requires you to vote for us on top.gg.
+This only takes a couple of seconds and helps support us. [vote](https://top.gg/bot/$clientID/vote)
 **Your rewards for voting:**
 +$getVar[Coi]**1000**]
-    $footer[Our privacy policy applies to this command: https://dankrpg.xyz/privacy]
-    $color[$getGlobalUserVar[EmbedColor;$authorID]]
-    $globalCooldown[1m;You haven't voted. Please try again.]
-    $else
-    Thank you for voting! Your support helps the bot grow :)
+$footer[Our privacy policy applies to this command: https://dankrpg.xyz/privacy]
+$color[$getGlobalUserVar[EmbedColor;$authorID]]
+$globalCooldown[1m;You haven't voted. Please try again.]
+$else
+Thank you for voting! Your support helps the bot grow :)
 +$getVar[Coi]**1000**
 $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];1000];$authorID]
 $globalCooldown[7d;You can only claim your voting rewards every week. You need to wait **%time%**.]
