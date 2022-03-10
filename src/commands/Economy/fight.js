1b2c41a87e614442db1c498485f7805e53788512
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-03-10 08:38:02 +0000

    Major bug fix (resetting everyone's vars. slight oops.)

--- src/commands/Economy/fight.js
+++ src/commands/Economy/fight.js
@@ -4,9 +4,9 @@ code: `
 $if[$getGlobalUserVar[ATK;$authorID]==1]
 $if[$getGlobalUserVar[HP;$authorID]<$random[1;5]]
 You die because you have run out of HP.
-$resetGlobalUserVar[HP]
-$resetGlobalUserVar[Coins]
-$resetGlobalUserVar[ATK]
+$setGlobalUserVar[HP;100:$authorID]
+$setGlobalUserVar[Coins;0;$authorID]
+$setGlobalUserVar[ATK;1;$authorID]
 $else
 
 $title[A new monster has spawned!]
@@ -22,9 +22,9 @@ $else
 $if[$getGlobalUserVar[ATK;$authorID]==2]
 $if[$getGlobalUserVar[HP;$authorID]<$random[5;10]]
 You die because you have run out of HP.
-$resetGlobalUserVar[HP]
-$resetGlobalUserVar[Coins]
-$resetGlobalUserVar[ATK]
+$setGlobalUserVar[HP;100:$authorID]
+$setGlobalUserVar[Coins;0;$authorID]
+$setGlobalUserVar[ATK;1;$authorID]
 $else
 $title[A new monster has spawned!]
 $description[You fight it, dealing **2 ATK**.\nIt dies, and you earn <:RPGCoin:855767372534906920>**$random[15;20]**.
@@ -39,9 +39,9 @@ $else
 $if[$getGlobalUserVar[ATK;$authorID]==3]
 $if[$getGlobalUserVar[HP;$authorID]<$random[10;15]]
 You die because you have run out of HP.
-$resetGlobalUserVar[HP]
-$resetGlobalUserVar[Coins]
-$resetGlobalUserVar[ATK]
+$setGlobalUserVar[HP;100:$authorID]
+$setGlobalUserVar[Coins;0;$authorID]
+$setGlobalUserVar[ATK;1;$authorID]
 $else
 $title[A new monster has spawned!]
 $description[You fight it, dealing **3 ATK**.\nIt dies, and you earn <:RPGCoin:855767372534906920>**$random[20;25]**.
@@ -55,9 +55,9 @@ $else
 $if[$getGlobalUserVar[ATK;$authorID]==4]
 $if[$getGlobalUserVar[HP;$authorID]<$random[15;20]]
 You die because you have run out of HP.
-$resetGlobalUserVar[HP]
-$resetGlobalUserVar[Coins]
-$resetGlobalUserVar[ATK]
+$setGlobalUserVar[HP;100:$authorID]
+$setGlobalUserVar[Coins;0;$authorID]
+$setGlobalUserVar[ATK;1;$authorID]
 $else
 $title[A new monster has spawned!]
 $description[You fight it, dealing **4 ATK**.\nIt dies, and you earn <:RPGCoin:855767372534906920>**$random[25;30]**.
@@ -72,9 +72,9 @@ $else
 $if[$getGlobalUserVar[ATK;$authorID]==5]
 $if[$getGlobalUserVar[HP;$authorID]<$random[20;25]]
 You die because you have run out of HP.
-$resetGlobalUserVar[HP]
-$resetGlobalUserVar[Coins]
-$resetGlobalUserVar[ATK]
+$setGlobalUserVar[HP;100:$authorID]
+$setGlobalUserVar[Coins;0;$authorID]
+$setGlobalUserVar[ATK;1;$authorID]
 $else
 $title[A new monster has spawned!]
 $description[You fight it, dealing **5 ATK**.\nIt dies, and you earn <:RPGCoin:855767372534906920>**$random[30;35]**.
