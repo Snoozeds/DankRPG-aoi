7a99714fb45406a5ea8e4fa5d106cb8f64fbf8b9
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-03-13 17:49:06 +0000

    Fix blacklist bug

--- src/commands/Common/blacklist.js
+++ src/commands/Common/blacklist.js
@@ -1,7 +1,11 @@
 module.exports = ({
     name: "blacklist",
     code: `
+    $if[$getVar[BlacklistedUsers]==]
+    $setVar[BlacklistedUsers;$getVar[BlacklistedUsers]$message]
+    $else
     $setVar[BlacklistedUsers;$getVar[BlacklistedUsers]+$message]
+    $endif
     $onlyForIDs[$botOwnerID[;];]
     $onlyIf[$isBot[$authorID]!=true;]`
     })
