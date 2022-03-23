daf28eb669f25dc7796ee4fa6ec48204a209aeee
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-03-23 16:13:45 +0000

    Fix whois error

--- src/commands/Common/whois.js
+++ src/commands/Common/whois.js
@@ -5,7 +5,7 @@ code: `
 $description[1;
 \`$username[$findUser[$message;yes]]#$discriminator[$findUser[$message;yes]]\`
 :bust_in_silhouette: **Highest Role:** <@&$highestRole[$findUser[$message;yes]]>
-<:Arrow_Right_Green:915175740947972097> **Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]> (<t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>)
+<:Arrow_Right_Green:915175740947972097> **Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]> (<t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>)
 :door: **Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]> (<t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>)
 ]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
