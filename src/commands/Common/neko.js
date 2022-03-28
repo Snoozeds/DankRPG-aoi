328f39740f334a710d6d159953c753bc2f66cd4b
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-03-28 00:09:52 +0000

    Add cooldown to neko

--- src/commands/Common/neko.js
+++ src/commands/Common/neko.js
@@ -6,5 +6,6 @@ $image[1;$httpRequest[https://api.nekos.rest/neko;GET;;url]]
 $description[1;[Source]($httpRequest[https://api.nekos.rest/neko;GET;;source]) | [Artist]($httpRequest[https://api.nekos.rest/neko;GET;;artist])
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $footer[1;Requested by $username | https://nekos.rest]
+$globalCooldown[7s;Chillax. Wait **%time%** before requesting again.]
 `
 }
