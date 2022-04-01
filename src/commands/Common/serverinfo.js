f7184ea857819b28ab9e3129714911976ceb3026
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-01 10:54:42 +0100

    Add servericon to serverinfo

--- src/commands/Common/serverinfo.js
+++ src/commands/Common/serverinfo.js
@@ -1,11 +1,13 @@
 module.exports = {
 name: "serverinfo",
+aliases: "si",
 code: `$description[1;
 \`$serverName ($guildID)\`
 :bust_in_silhouette: **Server Owner:** <@$ownerID>
 <:Boosted:917749329295454238> **Boost Count:** $serverBoostCount[$guildID] boosts
 :slight_smile: **Emoji Count:** $emojiCount[$guildID] emojis
 ]
+$thumbnail[1;$serverIcon[$guildID]]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
 $onlyIf[$isBot[$authorID]!=true;]`
