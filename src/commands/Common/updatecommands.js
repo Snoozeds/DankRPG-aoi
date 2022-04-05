96d5c5d6e81f14bf975899be3eb73751dd7dbf7a
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-05 21:00:45 +0100

    Add a reaction when refreshing

--- src/commands/Common/updatecommands.js
+++ src/commands/Common/updatecommands.js
@@ -3,5 +3,6 @@ name: "update",
 aliases: "refresh",
 code: `$onlyForIDs[$botOwnerID[;];]
 $updateCommands
+$addCmdReactions[✅]
 $onlyIf[$isBot[$authorID]!=true;]`
 } // This will NOT add new variables. It only refreshes each command file changed.
