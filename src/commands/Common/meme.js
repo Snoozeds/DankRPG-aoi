5e716f166dce633153aea567ff17e3d59fc4efc4
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-08 02:29:44 +0100

    Update req

--- src/commands/Common/meme.js
+++ src/commands/Common/meme.js
@@ -6,16 +6,16 @@ code: `
 $addButton[1;$replaceText[$getObjectProperty[numComments];undefined;0];primary;comments;yes;💬]
 $addButton[1;$replaceText[$getObjectProperty[downvotes];undefined;0];danger;downvotes;yes;⬇]
 $addButton[1;$replaceText[$getObjectProperty[upvotes];undefined;0];success;upvotes;yes;⬆]
-$footer[1;From r/$randomText[dankmemes;memes]]
+$footer[1;From r/memes]
 $title[1;$getObjectProperty[title];$getObjectProperty[url]]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $replaceText[$replaceText[$checkContains[$getObjectProperty[image];https://v.redd.it/];true;];false;$image[1;$getObjectProperty[image]]]
 $if[$checkContains[$getObjectProperty[image];https://v.redd.it/]==false]
-$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=$randomText[dankmemes;memes]]]
+$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=memes&limit=1&sort=top&t=day]]
 $endIf
 $if[$checkContains[$getObjectProperty[image];https://v.redd.it/]==false]
-$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=$randomText[dankmemes;memes]]]
+$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=memes&limit=1&sort=top&t=day]]
 $endIf
-$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=$randomText[dankmemes;memes]]]
+$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=memes&limit=1&sort=top&t=day]]
 $globalCooldown[5s;Please wait **%time%** to use this command again.]`
 })
