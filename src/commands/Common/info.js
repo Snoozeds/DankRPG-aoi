3821d768a22d64c9d541f46cb21ab2108fd7f17a
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-10 11:24:55 +0100

    Better the uptime in info

--- src/commands/Common/info.js
+++ src/commands/Common/info.js
@@ -5,7 +5,7 @@ $description[1;
 **__Bot Info__**
 Bot Ping: $round[$ping]ms
 Database Ping: $round[$dbPing]ms
-Uptime: $replaceText[$replaceText[$replaceText[$replaceText[$uptime; days,;d;1]; hours,;h;1]; minutes,;m;1]; seconds;s;1]
+Uptime: $replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$uptime; days,;d;1]; hours,;h;1]; minutes,;m;1]; seconds;s;1]; day,;d;1]; hour,;h;1]; minute,;m;1]; second;s;1]
 Server Count: $serverCount
 $username[$clientID] Invite: [link](https://discord.com/oauth2/authorize?client_id=$clientID&scope=bot%20applications.commands&permissions=2147863552) \n
 **__Server Info__**
