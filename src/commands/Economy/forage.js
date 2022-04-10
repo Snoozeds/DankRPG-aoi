3d976558cea487c919f3fd44386e655cdfcfe4da
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-10 11:16:26 +0100

    Increase forage amount

--- src/commands/Economy/forage.js
+++ src/commands/Economy/forage.js
@@ -1,8 +1,8 @@
 module.exports = {
 name: "forage",
 aliases: ["explore", "search"],
-code: `$description[1;<@$authorID> goes foraging. They find: $randomText[Wood;Stone;Slime] (**$random[1;3]**)]
-$setGlobalUserVar[$randomText[Wood;Stone;Slime];$sum[$getGlobalUserVar[$randomText[Wood;Stone;Slime]];$random[1;3]];$authorID]
+code: `$description[1;<@$authorID> goes foraging. They find: $randomText[Wood;Stone;Slime] (**$random[15;20]**)]
+$setGlobalUserVar[$randomText[Wood;Stone;Slime];$sum[$getGlobalUserVar[$randomText[Wood;Stone;Slime]];$random[15;20]];$authorID]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $globalCooldown[10s;Please wait **%time%**]
 $onlyIf[$isBot[$authorID]!=true;]
