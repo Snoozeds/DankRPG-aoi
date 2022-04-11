13e9ea7c9e5d3b7895357935969c00d392642fdf
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-11 09:53:04 +0100

    Fix gift's horrible code

--- src/commands/Economy/gift.js
+++ src/commands/Economy/gift.js
@@ -2,17 +2,18 @@ module.exports = {
 name: "gift",
 aliases: "give",
 code: `
-$description[1;You gifted $getVar[Coi]$message[2] to <@$mentioned[1]>]
+$description[1;You gifted $getVar[Coi]$message[2] to <@$findUser[$message[1];no]>.]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
+$footer[1;Gift from $username[$authorID]]
 
 $setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins;$authorID];$message[2]];$authorID]
-$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$mentioned[1]];$message[2]];$mentioned[1]]
+$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$findUser[$message[1];no]];$message[2]];$findUser[$message[1];no]]
 
-$onlyIf[$findUser[$mentioned[1]]!=$authorID;You cannot give coins to yourself.]
+$onlyIf[$findUser[$message[1];no]!=$authorID;You cannot give coins to yourself.]
 $onlyIf[$message!=;**Usage**: \`$getServerVar[Prefix]give <@username> <amount>\` (error: no user mentioned, no amount mentioned)]
 $onlyIf[$isNumber[$message[2]]!=false;**Usage**: \`$getServerVar[Prefix]give <@username> <amount>\` (error: not a number)]
 $onlyIf[$checkContains[$message[2];-]!=true;No negative values allowed.]
-$onlyIf[$message[2]>$getGlobalUserVar[Coins;$authorID];You don't have this much.]
+$onlyIf[$message[2]<=$getGlobalUserVar[Coins;$authorID];You don't have this much.]
 $globalCooldown[5s;I'm sure you don't need to give $username[$mentioned[1]] money all the time :upside_down: (**%time%**)]
 $suppressErrors
 $onlyIf[$isBot[$authorID]!=true;]
