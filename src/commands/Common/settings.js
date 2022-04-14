3d91770edd642038303c1a763e3733c439b8d2b9
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-14 10:30:56 +0100

    Fix settings embed

--- src/commands/Common/settings.js
+++ src/commands/Common/settings.js
@@ -1,44 +1,44 @@
 module.exports = [{
-    name: "settings",
-    aliases: "options",
-    code: `
-    $title[1;Settings]
-    $description[1;Here are all the settings you can change.
+name: "settings",
+aliases: "options",
+code: `
+$title[1;Settings]
+$description[1;Here are all the settings you can change.
 
-    **Embed color** - This will change the color of embeds to your own choice. [Color Picker](https://colorpicker.me 'Color Picker')
-		$getServerVar[Prefix]changecolor <#hexid>
-		Your value: $getGlobalUserVar[EmbedColor;$authorID]\n
+**Embed color** - This will change the color of embeds to your own choice. [Color Picker](https://colorpicker.me 'Color Picker')
+$getServerVar[Prefix]changecolor <#hexid>
+Your value: $getGlobalUserVar[EmbedColor;$authorID]\n
 
-    **Family Mode** - Removes swearing/references from commands. 
-		$getServerVar[Prefix]familymode <true/false>
-		Your Value: $getGlobalUserVar[FamilyMode;$authorID]\n
+**Family Mode** - Removes swearing/references from commands. 
+$getServerVar[Prefix]familymode <true/false>
+Your Value: $getGlobalUserVar[FamilyMode;$authorID]\n
 
-    **UserInteractions** - Whether or not users can use commands that mention you (e.g hug, marry).
-		$getServerVar[Prefix]userint <true/false>
-		Your value: $getGlobalUserVar[UserInt;$authorID] \n
+**UserInteractions** - Whether or not users can use commands that mention you (e.g hug, marry).
+$getServerVar[Prefix]userint <true/false>
+Your value: $getGlobalUserVar[UserInt;$authorID] \n
 
-    **Cat Notifications** - Whether or not cat notifications are send in chat (finding coins).
-    $getServerVar[Prefix]catnotifications <true/false>
-    Your value: $getGlobalUserVar[CatNotificationsOn;$authorID]]
-		$footer[1;True = Enabled | False = Disabled]
+**Cat Notifications** - Whether or not cat notifications are send in chat (finding coins).
+$getServerVar[Prefix]catnotifications <true/false>
+Your value: $getGlobalUserVar[CatNotificationsOn;$authorID]]
+$footer[1;True = Enabled | False = Disabled]
 
-    $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-    $globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
-		$suppressErrors
-    $onlyIf[$isBot[$authorID]!=true;]`
+$color[1;$getGlobalUserVar[EmbedColor;$authorID]]
+$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
+$suppressErrors
+$onlyIf[$isBot[$authorID]!=true;]`
 },
 
 {
-    name : "changecolor",
-    aliases: "changecolour",
-    code: `
-    $onlyif[$isValidHex[$replaceText[$message;#;;-1]]!=false;This is not a valid hex code. Please visit <https://colorpicker.me> to find a valid hex code.]
-    Changed all embed colors to #$replaceText[$message;#;;-1].
-		$setGlobalUserVar[EmbedColor;$message;$authorID]
-    $globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
-		$suppressErrors
-    $onlyIf[$isBot[$authorID]!=true;]
-    `
+name : "changecolor",
+aliases: "changecolour",
+code: `
+$onlyif[$isValidHex[$replaceText[$message;#;;-1]]!=false;This is not a valid hex code. Please visit <https://colorpicker.me> to find a valid hex code.]
+Changed all embed colors to #$replaceText[$message;#;;-1].
+$setGlobalUserVar[EmbedColor;$message;$authorID]
+$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
+$suppressErrors
+$onlyIf[$isBot[$authorID]!=true;]
+`
 },
 
 {
