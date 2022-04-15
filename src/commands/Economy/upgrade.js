13b2c7c73b53bd574dc7fb8ff1bbd75bda13670c
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-15 09:47:41 +0100

    re-align upgrade.js

--- src/commands/Economy/upgrade.js
+++ src/commands/Economy/upgrade.js
@@ -13,41 +13,40 @@ module.exports = {
 	$onlyIf[$getGlobalUserVar[Coins;$authorID]>499;You do not have enough <:RPGCoin:855767372534906920> to upgrade. You need at least <:RPGCoin:855767372534906920>**500**.]
 	$else
 
-	$if[$getGlobalUserVar[ATK;$authorID]==2]
-	You upgraded to 3ATK for <:RPGCoin:855767372534906920>**1000**.
-	$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];1000];$authorID]
-	$setGlobalUserVar[ATK;3;$authorID]
-	$onlyIf[$getGlobalUserVar[Coins;$authorID]>999;You do not have enough <:RPGCoin:855767372534906920> to upgrade. You need at least <:RPGCoin:855767372534906920>**1000**.]
-	$setGlobalUserVar[ATKAchievement;<:Unlocked:899050875719393281>]
+$if[$getGlobalUserVar[ATK;$authorID]==2]
+You upgraded to 3ATK for <:RPGCoin:855767372534906920>**1000**.
+$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];1000];$authorID]
+$setGlobalUserVar[ATK;3;$authorID]
+$onlyIf[$getGlobalUserVar[Coins;$authorID]>999;You do not have enough <:RPGCoin:855767372534906920> to upgrade. You need at least <:RPGCoin:855767372534906920>**1000**.]
+$setGlobalUserVar[ATKAchievement;<:Unlocked:899050875719393281>]
 
-	$else
-	$if[$getGlobalUserVar[ATK;$authorID]==3]
-	You upgraded to 4ATK for <:RPGCoin:855767372534906920>**1500**.
-	$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];1500];$authorID]
-	$setGlobalUserVar[ATK;4;$authorID]
-	$onlyIf[$getGlobalUserVar[Coins;$authorID]>1499;You do not have enough <:RPGCoin:855767372534906920> to upgrade. You need at least <:RPGCoin:855767372534906920>**1500**.]
-	$setGlobalUserVar[ATKAchievement;<:Unlocked:899050875719393281>]
+$else
+$if[$getGlobalUserVar[ATK;$authorID]==3]
+You upgraded to 4ATK for <:RPGCoin:855767372534906920>**1500**.
+$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];1500];$authorID]
+$setGlobalUserVar[ATK;4;$authorID]
+$onlyIf[$getGlobalUserVar[Coins;$authorID]>1499;You do not have enough <:RPGCoin:855767372534906920> to upgrade. You need at least <:RPGCoin:855767372534906920>**1500**.]
+$setGlobalUserVar[ATKAchievement;<:Unlocked:899050875719393281>]
 
-	$else
-	$if[$getGlobalUserVar[ATK;$authorID]==4]
-	You upgraded to 5ATK for <:RPGCoin:855767372534906920>**2000**.
-	$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];2000];$authorID]
-	$setGlobalUserVar[ATK;5;$authorID]
-	$onlyIf[$getGlobalUserVar[Coins;$authorID]>1999;You do not have enough <:RPGCoin:855767372534906920> to upgrade. You need at least <:RPGCoin:855767372534906920>**2000**.]
-	$setGlobalUserVar[ATKAchievement;<:Unlocked:899050875719393281>]
+$else
+$if[$getGlobalUserVar[ATK;$authorID]==4]
+You upgraded to 5ATK for <:RPGCoin:855767372534906920>**2000**.
+$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];2000];$authorID]
+$setGlobalUserVar[ATK;5;$authorID]
+$onlyIf[$getGlobalUserVar[Coins;$authorID]>1999;You do not have enough <:RPGCoin:855767372534906920> to upgrade. You need at least <:RPGCoin:855767372534906920>**2000**.]
+$setGlobalUserVar[ATKAchievement;<:Unlocked:899050875719393281>]
 
-	$else
-	$if[$getGlobalUserVar[ATK;$authorID]==5]
-	You have the max ATK. Congrats!
-	$setGlobalUserVar[ATKAchievement;<:Unlocked:899050875719393281>]
+$else
+$if[$getGlobalUserVar[ATK;$authorID]==5]
+You have the max ATK. Congrats!
+$setGlobalUserVar[ATKAchievement;<:Unlocked:899050875719393281>]
 
-	$endif
-	$endif
-	$endif
-	$endif
-	$endif
-	$suppressErrors
-	$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
-	$onlyIf[$isBot[$authorID]!=true;]
+$endif
+$endif
+$endif
+$endif
+$endif
+$suppressErrors
+$globalCooldown[10s;Please wait **%time%** before running this command again. This helps us enforce ratelimits.]
 	`
 }
