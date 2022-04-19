98cc6d68916b759b14febf5167829f199c5ee641
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-19 15:12:34 +0100

    Nerf mine sell rates + Add pickaxe reminder.

--- src/commands/Economy/mine.js
+++ src/commands/Economy/mine.js
@@ -8,7 +8,7 @@ $setGlobalUserVar[Gold;$sum[$getGlobalUserVar[Gold;$authorID];$random[5;10]];$au
 $setGlobalUserVar[Diamond;$sum[$getGlobalUserVar[Diamond;$authorID];$random[1;5]];$authorID]
 $setGlobalUserVar[Stone;$sum[$getGlobalUserVar[Stone;$authorID];$random[10;20]];$authorID]
 $wait[10m]
-$channelSendMessage[$channelID;You have started mining. Come back in $getGlobalUserVar[MiningTime;$authorID] to see what you have mined.]
+$channelSendMessage[$channelID;You have started mining. Come back in $getGlobalUserVar[MiningTime;$authorID] to see what you have mined.\n> To make your mining faster, you can now buy a pickaxe. \`$getServerVar[Prefix]shop\`]
 
 $else
 $if[$getGlobalUserVar[MiningTime;$authorID]==5m]
@@ -33,15 +33,15 @@ code: `
 $setGlobalUserVar[Gold;0;$authorID]
 $setGlobalUserVar[Diamond;0;$authorID]
 $setGlobalUserVar[Stone;0;$authorID]
-$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$authorID];$sum[$multi[$getGlobalUserVar[Stone;$authorID];5];$multi[$getGlobalUserVar[Gold;$authorID];30];$multi[$getGlobalUserVar[Diamond;$authorID];40]]];$authorID]
+$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$authorID];$sum[$multi[$getGlobalUserVar[Stone;$authorID];3];$multi[$getGlobalUserVar[Gold;$authorID];20];$multi[$getGlobalUserVar[Diamond;$authorID];30]]];$authorID]
 $wait[1s]
 You sold:
 $getVar[DiamondEmoji]**$getGlobalUserVar[Diamond]**
 $getVar[GoldEmoji]**$getGlobalUserVar[Gold]**
 $getVar[StoneEmoji]**$getGlobalUserVar[Stone]**
-For: $getVar[Coi]**$sum[$multi[$getGlobalUserVar[Stone;$authorID];5];$multi[$getGlobalUserVar[Gold;$authorID];30];$multi[$getGlobalUserVar[Diamond;$authorID];40]]**
+For: $getVar[Coi]**$sum[$multi[$getGlobalUserVar[Stone;$authorID];3];$multi[$getGlobalUserVar[Gold;$authorID];20];$multi[$getGlobalUserVar[Diamond;$authorID];30]]**
 $onlyIf[$isBot[$authorID]!=true;]
-$onlyIf[$sum[$multi[$getGlobalUserVar[Stone;$authorID];5];$multi[$getGlobalUserVar[Gold;$authorID];30];$multi[$getGlobalUserVar[Diamond;$authorID];40]]!=0;You have nothing to sell. Try \`$getServerVar[Prefix;$guildID]mine\` to mine some resources.]
+$onlyIf[$sum[$multi[$getGlobalUserVar[Stone;$authorID];3];$multi[$getGlobalUserVar[Gold;$authorID];20];$multi[$getGlobalUserVar[Diamond;$authorID];30]]!=0;You have nothing to sell. Try \`$getServerVar[Prefix;$guildID]mine\` to mine some resources.]
 $suppressErrors[Something went wrong. You probably have nothing to sell.]
 `
 }] // I add a wait to this one because otherwise it'll say that you sold 0 of everything... idk, aoi.js is weird.
