83d7b0c95a07c291f036bb14e55a0b165208afc4
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-19 14:50:11 +0100

    change footer for neko.js

--- src/commands/Common/neko.js
+++ src/commands/Common/neko.js
@@ -5,7 +5,7 @@ $title[1;🐱]
 $image[1;$httpRequest[https://api.nekos.rest/neko;GET;;url]]
 $description[1;[Source]($httpRequest[https://api.nekos.rest/neko;GET;;source]) | [Artist]($httpRequest[https://api.nekos.rest/neko;GET;;artist])
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$footer[1;Requested by $username | https://nekos.rest]
+$footer[1;api: nekos.rest]
 $globalCooldown[5s;Chillax. Wait **%time%** before requesting again.]
 `
 }
