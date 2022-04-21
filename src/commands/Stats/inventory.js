95a16b971fd0e9cc37346efebeacaeb6589b1def
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-21 21:57:18 +0100

    Fix big gaps in inventory

--- src/commands/Stats/inventory.js
+++ src/commands/Stats/inventory.js
@@ -1,26 +1,11 @@
 module.exports = {
 name: "inventory",
-$if: "v4",
 aliases: ["inv", "i"],
 code: `$title[1;$username[$findUser[$message;yes]]'s Inventory]
 $description[1;
-$if[$getGlobalUserVar[Wood;$findUser[$message;yes]]!=0]
-**Wood:** $getGlobalUserVar[Wood;$findUser[$message;yes]]
-$endif
-$if[$getGlobalUserVar[Slime;$findUser[$message;yes]]!=0]
-**Slime:** $getGlobalUserVar[Slime;$findUser[$message;yes]]
-$endif
-$if[$getGlobalUserVar[Stone;$findUser[$message;yes]]!=0]
-**Stone:** $getGlobalUserVar[Stone;$findUser[$message;yes]]
-$endif
-$if[$getGlobalUserVar[Gold;$findUser[$message;yes]]!=0]
-**Gold:** $getGlobalUserVar[Gold;$findUser[$message;yes]]
-$endif
-$if[$getGlobalUserVar[Diamond;$findUser[$message;yes]]!=0]
-**Diamonds:** $getGlobalUserVar[Diamond;$findUser[$message;yes]]
-$endif
+$if[$getGlobalUserVar[Wood;$findUser[$message;yes]]!=0;**Wood:** $getGlobalUserVar[Wood;$findUser[$message;yes]]] $if[$getGlobalUserVar[Slime;$findUser[$message;yes]]!=0;\n**Slime:** $getGlobalUserVar[Slime;$findUser[$message;yes]]] $if[$getGlobalUserVar[Stone;$findUser[$message;yes]]!=0;\n**Stone:** $getGlobalUserVar[Stone;$findUser[$message;yes]]] $if[$getGlobalUserVar[Gold;$findUser[$message;yes]]!=0;\n**Gold:** $getGlobalUserVar[Gold;$findUser[$message;yes]]] $if[$getGlobalUserVar[Diamond;$findUser[$message;yes]]!=0;\n**Diamonds:** $getGlobalUserVar[Diamond;$findUser[$message;yes]]] 
 ]
 $color[1;$getGlobalUserVar[EmbedColor]]
-$globalCooldown[5s;Please wait **%time%**.]
-$onlyIf[$isBot[$authorID]!=true;]`
+$globalCooldown[3s;Please wait **%time%**.]
+`
 }
