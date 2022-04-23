7e01ef316002c9171ecf3b4e714de45b41f03441
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-23 21:38:22 +0100

    Change hpDrop stock

--- src/commands/Shop/shop.js
+++ src/commands/Shop/shop.js
@@ -4,7 +4,7 @@ $if: "v4",
 code: `
 $title[1;Welcome to my shop!]
 $description[1;Here are my items on sale.\n
-<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your HP by 100. | <:RPGCoin:855767372534906920>**750**\n**In stock: ∞**\n$getServerVar[Prefix]buyhp \n
+<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your HP by 100. | <:RPGCoin:855767372534906920>**750**\n**In stock: $round[$divide[$sub[1000;$getGlobalUserVar[MaxHP]];100]]**\n$getServerVar[Prefix]buyhp \n
 $if[$getGlobalUserVar[PickL;$authorID]==1] \n<:pickaxe:963846459126583347> **__Pickaxe__** \nDecreases your mining speed by 5m. | **100 Stone** \n**In stock: $getGlobalUserVar[PickL;$authorID]** \n$getServerVar[Prefix]buypick \n\n $endif $if[$getGlobalUserVar[BaitL;$authorID]==1] \n<:Bait:916369932072128544> **__Bait__** \nFind double the animals in \`$getServerVar[Prefix]hunt\`. | **100 Slime** \n**In stock: $getGlobalUserVar[BaitL;$authorID]** \n$getServerVar[Prefix]buybait \n\n $endif
 Other Shop commands:\n\`upgrade\`, \`hp\`, \`heal\`, \`petshop\`]
 $color[1;$getGlobalUserVar[EmbedColor]]
