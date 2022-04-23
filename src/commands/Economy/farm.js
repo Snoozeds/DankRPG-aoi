204ecf024d5a4867dba988d62c7ab3b5b3455caf
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-23 22:52:37 +0100

    Add dying function

--- src/commands/Economy/farm.js
+++ src/commands/Economy/farm.js
@@ -2,9 +2,14 @@ module.exports = {
 name: "farm",
 $if: "v4",
 code: `
-	
-$if[$getGlobalUserVar[FirstFarm;$authorID]==False]
+$if[$getGlobalUserVar[HP;$authorID]<=35]
+You die because you have run out of HP.
+$setGlobalUserVar[HP;100;$authorID]
+$setGlobalUserVar[Coins;0;$authorID]
+$setGlobalUserVar[ATK;1;$authorID]
+$else
 
+$if[$getGlobalUserVar[FirstFarm;$authorID]==False]
 $if[$getGlobalUserVar[HP;$authorID]<$sub[$getGlobalUserVar[MaxHP;$authorID];50]]
 $title[1;$username's farm]
 $description[1;You farmed $getVar[FarmK]$getVar[FarmO]$getVar[FarmS] and earned <:RPGCoin:855767372534906920>**$random[25;50]**.\nYou fruit healed you for **$getVar[Heart]$random[25;35]**. You now have $getVar[Heart]**$getGlobalUserVar[HP;$authorID]/$getGlobalUserVar[MaxHP;$authorID]**.]
@@ -27,6 +32,7 @@ $setGlobalUserVar[FirstFarm;False]
 $setGlobalUserVar[FarmAchievement;<:Unlocked:899050875719393281>]
 $setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins];50]]
 $setGlobalUserVar[FirstFarm;False]
+$endif
 $endif
 $endif
 $globalCooldown[30s;Please wait **%time%** before farming again.]
