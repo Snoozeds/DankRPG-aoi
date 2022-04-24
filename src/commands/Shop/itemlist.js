5127c0d84166932c7054365ed04a5c7cfe9103a7
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-24 11:13:41 +0100

    Added wood and slime to the itemlist

--- src/commands/Shop/itemlist.js
+++ src/commands/Shop/itemlist.js
@@ -4,6 +4,8 @@ aliases: ["listitems", "items", "itemlist", "itemslist"],
 code: `
 $title[1;Items]
 $description[1;List of items:
+$addField[1;**__Wood:__**;Sell price: $getVar[Coi]**1**]
+$addField[1;$getVar[SlimeEmoji]**__Slime:__**;Sell price: $getVar[Coi]**2**]
 $addField[1;$getVar[StoneEmoji]**__Stone:__**;Sell price: $getVar[Coi]**3**]
 $addField[1;$getVar[GoldEmoji]**__Gold:__**;Sell price: $getVar[Coi]**20**]
 $addField[1;$getVar[DiamondEmoji]**__Diamond:__**;Sell price: $getVar[Coi]**30**]
