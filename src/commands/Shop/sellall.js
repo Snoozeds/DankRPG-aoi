240564a79ca8d9e6a1bd683b4637a852c0d65007
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-24 10:58:57 +0100

    You can now sell slime and wood.

--- src/commands/Shop/sellall.js
+++ src/commands/Shop/sellall.js
@@ -10,10 +10,11 @@ $setGlobalUserVar[foxStock;0;$authorID]
 $setGlobalUserVar[deerStock;0;$authorID]
 $setGlobalUserVar[mooseStock;0;$authorID]
 $setGlobalUserVar[rabbitStock;0;$authorID]
-$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$authorID];$sum[$multi[$getGlobalUserVar[Stone;$authorID];3];$multi[$getGlobalUserVar[Gold;$authorID];20];$multi[$getGlobalUserVar[Diamond;$authorID];30];$multi[$getGlobalUserVar[foxStock;$authorID];50];$multi[$getGlobalUserVar[deerStock;$authorID];75];$multi[$getGlobalUserVar[mooseStock;$authorID];100];$multi[$getGlobalUserVar[rabbitStock;$authorID];125]]];$authorID]
+$setGlobalUserVar[Wood;0;$authorID]
+$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$authorID];$sum[$multi[$getGlobalUserVar[Stone;$authorID];3];$multi[$getGlobalUserVar[Gold;$authorID];20];$multi[$getGlobalUserVar[Diamond;$authorID];30];$multi[$getGlobalUserVar[foxStock;$authorID];50];$multi[$getGlobalUserVar[deerStock;$authorID];75];$multi[$getGlobalUserVar[mooseStock;$authorID];100];$multi[$getGlobalUserVar[rabbitStock;$authorID];125];$multi[$getGlobalUserVar[Wood;$authorID];1];$multi[$getGlobalUserVar[Slime;$authorID];2]]];$authorID]
 
 $wait[1s]
-You sold **$sum[$getGlobalUserVar[foxStock];$sum[$getGlobalUserVar[deerStock];$getGlobalUserVar[mooseStock];$getGlobalUserVar[rabbitStock];$getGlobalUserVar[Stone;$authorID];$getGlobalUserVar[Gold;$authorID];$getGlobalUserVar[Diamond;$authorID]]] items** for $getVar[Coi]**$sum[$multi[$getGlobalUserVar[Stone;$authorID];3];$multi[$getGlobalUserVar[Gold;$authorID];20];$multi[$getGlobalUserVar[Diamond;$authorID];30];$multi[$getGlobalUserVar[foxStock;$authorID];50];$multi[$getGlobalUserVar[deerStock;$authorID];75];$multi[$getGlobalUserVar[mooseStock;$authorID];100];$multi[$getGlobalUserVar[rabbitStock;$authorID];125]]**
+$username[$authorID]: You sold **$sum[$getGlobalUserVar[foxStock];$sum[$getGlobalUserVar[deerStock];$getGlobalUserVar[mooseStock];$getGlobalUserVar[rabbitStock];$getGlobalUserVar[Stone;$authorID];$getGlobalUserVar[Gold;$authorID];$getGlobalUserVar[Diamond;$authorID];$getGlobalUserVar[Wood;$authorID];$getGlobalUserVar[Slime;$authorID]]] items** for $getVar[Coi]**$sum[$multi[$getGlobalUserVar[Stone;$authorID];3];$multi[$getGlobalUserVar[Gold;$authorID];20];$multi[$getGlobalUserVar[Diamond;$authorID];30];$multi[$getGlobalUserVar[foxStock;$authorID];50];$multi[$getGlobalUserVar[deerStock;$authorID];75];$multi[$getGlobalUserVar[mooseStock;$authorID];100];$multi[$getGlobalUserVar[rabbitStock;$authorID];125];$multi[$getGlobalUserVar[Wood;$authorID];1]]**
 $suppressErrors[Something went wrong. You probably have nothing to sell.]
 `
 }
