25a1dad398144391b971e9539545594c4ec44f47
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-25 14:32:41 +0100

    Cap hp properly

--- src/commands/Shop/hpDrop.js
+++ src/commands/Shop/hpDrop.js
@@ -6,7 +6,7 @@ You bought a hp drop for <:RPGCoin:855767372534906920>**750**.
 $setGlobalUserVar[MaxHP;$sum[$getGlobalUserVar[MaxHP];100];$authorID]
 $setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins];750]]
 $onlyIf[$getGlobalUserVar[Coins;$authorID]>749;You do not have enough <:RPGCoin:855767372534906920> to buy a **hpDrop**. You need at least <:RPGCoin:855767372534906920>**750**.]
-$onlyIf[$getGlobalUserVar[MaxHP;$authorID]!=1000;You have the maximum amount of hp.]
+$onlyIf[$getGlobalUserVar[MaxHP;$authorID]<=1000;You have the maximum amount of hp.]
 $suppressErrors
 $onlyIf[$isBot[$authorID]!=true;]
 `
