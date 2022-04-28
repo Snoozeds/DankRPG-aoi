2c6b1b6839ae6c88a16abac0636a6158dfbddf45
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-28 10:45:55 +0100

    Fix upvote emote.

--- src/commands/Common/meme.js
+++ src/commands/Common/meme.js
@@ -6,6 +6,6 @@ $title[1;$jsonRequest[https://meme-api.herokuapp.com/gimme;title]]
 $image[1;$jsonRequest[https://meme-api.herokuapp.com/gimme;url]]
 $description[1;[Source]($jsonRequest[https://meme-api.herokuapp.com/gimme;postLink])]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$footer[1;<:updoot:969165862651170837>$jsonRequest[https://meme-api.herokuapp.com/gimme;ups]]
+$footer[1;$jsonRequest[https://meme-api.herokuapp.com/gimme;ups] upvotes.]
 $globalCooldown[7s;Please slow down. **%time%**.]
 `};
