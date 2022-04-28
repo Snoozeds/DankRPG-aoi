ba6f5dd98b9393ccb068b0081345872806ef3242
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-28 10:45:00 +0100

    Change meme variety

--- src/commands/Common/meme.js
+++ src/commands/Common/meme.js
@@ -2,11 +2,10 @@ module.exports = {
 name : "meme",
 aliases: "memes",
 code : `
-$title[1;$jsonRequest[https://meme-api.herokuapp.com/gimme/memes;title]]
-$image[1;$jsonRequest[https://meme-api.herokuapp.com/gimme/memes;url]]
-$description[1;[Source]($jsonRequest[https://meme-api.herokuapp.com/gimme/memes;postLink]). \n<:updoot:969165862651170837> **$jsonRequest[https://meme-api.herokuapp.com/gimme/memes;ups]**]
+$title[1;$jsonRequest[https://meme-api.herokuapp.com/gimme;title]]
+$image[1;$jsonRequest[https://meme-api.herokuapp.com/gimme;url]]
+$description[1;[Source]($jsonRequest[https://meme-api.herokuapp.com/gimme;postLink])]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$footer[1;Requested by $username[$authorID].]
-$addTimestamp[1]
+$footer[1;<:updoot:969165862651170837>$jsonRequest[https://meme-api.herokuapp.com/gimme;ups]]
 $globalCooldown[7s;Please slow down. **%time%**.]
 `};
