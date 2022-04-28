79d2ded0033caf0116c738aa185bbd1272163059
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-04-28 10:24:09 +0100

    Change meme api.

--- src/commands/Common/meme.js
+++ src/commands/Common/meme.js
@@ -1,21 +1,12 @@
-module.exports = ({
-name: "meme",
+module.exports = {
+name : "meme",
 aliases: "memes",
-$if: "v4",
-code: `
-$addButton[1;$replaceText[$getObjectProperty[numComments];undefined;0];primary;comments;yes;💬]
-$addButton[1;$replaceText[$getObjectProperty[downvotes];undefined;0];danger;downvotes;yes;⬇]
-$addButton[1;$replaceText[$getObjectProperty[upvotes];undefined;0];success;upvotes;yes;⬆]
-$footer[1;From r/memes]
-$title[1;$getObjectProperty[title];$getObjectProperty[url]]
+code : `
+$title[1;$jsonRequest[https://meme-api.herokuapp.com/gimme/memes;title]]
+$image[1;$jsonRequest[https://meme-api.herokuapp.com/gimme/memes;url]]
+$description[1;[Source]($jsonRequest[https://meme-api.herokuapp.com/gimme/memes;postLink]). \n<:updoot:969165862651170837> **$jsonRequest[https://meme-api.herokuapp.com/gimme/memes;ups]**]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$replaceText[$replaceText[$checkContains[$getObjectProperty[image];https://v.redd.it/];true;];false;$image[1;$getObjectProperty[image]]]
-$if[$checkContains[$getObjectProperty[image];https://v.redd.it/]==false]
-$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=memes]]
-$endIf
-$if[$checkContains[$getObjectProperty[image];https://v.redd.it/]==false]
-$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=memes]]
-$endIf
-$createObject[$jsonRequest[https://cryptons-api.herokuapp.com/api/v1/reddit?subreddit=memes]]
-$globalCooldown[5s;Please wait **%time%** to use this command again.]`
-})
+$footer[1;Requested by $username[$authorID].]
+$addTimestamp[1]
+$globalCooldown[7s;Please slow down. **%time%**.]
+`};
