a8644790c20cef220e5a7a64ef9caf73b6e24617
Snoozeds <talktomrdeluxe@gmail.com>
2022-05-20 10:03:24 +0100

    prevent spam pinging drpg

--- src/commands/Common/noprefix.js
+++ src/commands/Common/noprefix.js
@@ -3,5 +3,6 @@ name: "<@855479925863481345>",
 nonPrefixed: "true",
 code: `Hello! my prefix for this server is: \`$getServerVar[Prefix;$guildID]\`
 You can also ping me instead of using a prefix.
+$globalCooldown[10s;]
 `
 }
