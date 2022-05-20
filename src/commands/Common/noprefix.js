f2b5cf285091d0f80793799d12c607e3b18c13bf
Snoozeds <talktomrdeluxe@gmail.com>
2022-05-20 10:02:41 +0100

    You can now ping dankrpg to see the prefix.

---
+++ src/commands/Common/noprefix.js
@@ -0,0 +1,7 @@
+module.exports = {
+name: "<@855479925863481345>",
+nonPrefixed: "true",
+code: `Hello! my prefix for this server is: \`$getServerVar[Prefix;$guildID]\`
+You can also ping me instead of using a prefix.
+`
+}
