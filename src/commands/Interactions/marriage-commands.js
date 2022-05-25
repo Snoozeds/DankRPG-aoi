14628692716fd710e746e8b3a5a8de206a72f8e9
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-05-25 14:43:42 +0100

    change the overall look of d!marriage

--- src/commands/Interactions/marriage-commands.js
+++ src/commands/Interactions/marriage-commands.js
@@ -42,9 +42,10 @@ $onlyIf[$getGlobalUserVar[InRelationship;$authorID]!=True;You are already marrie
 name: "marriage",
 aliases: ["marriageinfo"],
 code: `
-$title[1;🌸 | Marriage info: $username[$findUser[$message;yes]]]
+$title[1;🌸 | Marriage info: $nickname[$findUser[$message;yes]]]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$description[1;<@$findUser[$message;yes]> is married to: <@$getGlobalUserVar[MarriedTo;$findUser[$message;yes]]>\nMarried since: <t:$getGlobalUserVar[MarriageDate;$findUser[$message;yes]]> (<t:$getGlobalUserVar[MarriageDate;$findUser[$message;yes]]:R>)]
+$description[1;<@$findUser[$message;yes]> is married to: <@$getGlobalUserVar[MarriedTo;$findUser[$message;yes]]>\nMarried since: <t:$getGlobalUserVar[MarriageDate;$findUser[$message;yes]]:R>]
+$thumbnail[1;$userAvatar[$findUser[$message;yes]]]
 $onlyIf[$getGlobalUserVar[InRelationship;$findUser[$message;yes]]!=False;This user is not married.]
 $onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has interaction commands disabled from their settings.]
 $onlyIf[$isBot[$findUser[$message;yes]]!=true;Bots marrying people... hmm.]
