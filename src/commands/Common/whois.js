2eaf99df87467cd2729ec86cf8c570c27f90ff34
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-06-06 13:30:17 +0100

    Simplify whois.

--- src/commands/Common/whois.js
+++ src/commands/Common/whois.js
@@ -4,9 +4,9 @@ aliases: ["wi", "ui", "userinfo"],
 code: `
 $description[1;
 \`$username[$findUser[$message;yes]]#$discriminator[$findUser[$message;yes]]\`
-:bust_in_silhouette: **Highest Role:** <@&$highestRole[$findUser[$message;yes]]>
-<:Arrow_Right_Green:915175740947972097> **Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
-:door: **Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
+**Highest Role:** <@&$highestRole[$findUser[$message;yes]]>
+**Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
+**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
 ]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $thumbnail[1;$userAvatar[$findUser[$message;yes]]]
