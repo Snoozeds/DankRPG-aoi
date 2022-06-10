a0e016430db1b455a5d629657c9d3c4e0e6de8ef
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-06-10 21:53:39 +0100

    New command: gamble/bet

--- src/commands/Common/commands.js
+++ src/commands/Common/commands.js
@@ -7,7 +7,7 @@ $color[1;$getGlobalUserVar[EmbedColor]]
 $description[1;
 **Economy Commands**:
 \`daily, fight, farm, forage, slots, dungeon, mine,
-give, sellall, pethunt, hunt, blackjack\`
+give, sellall, pethunt, hunt, blackjack, gamble\`
 
 **Social Commands**:
 \`hug, kill, slap, wave, laugh, marry, marriage,
---
+++ src/commands/Economy/bet.js
@@ -0,0 +1,39 @@
+module.exports = {
+name: "bet",
+aliases: "gamble",
+$if: "v4",
+code: `
+
+$if[$randomText[Win;Lose;Lose]==Win]
+$title[1;You won your bet.]
+$color[1;#7CFC00]
+$description[1;**$username[$authorID]**'s bet
++ $getVar[Coi]$multi[$message[1];2]
+
+**Your new balance: $getVar[Coi]$getGlobalUserVar[Coins;$authorID]**
+]
+$footer[1;Chance of winning: 1/3]
+$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$authorID];$multi[$message[1];2]]]
+
+$else
+$title[1;You lost your bet.]
+$color[1;#FF0000]
+$description[1;**$username[$authorID]**'s bet
+- $getVar[Coi]$message[1]
+
+**Your new balance: $getVar[Coi]$getGlobalUserVar[Coins;$authorID]**
+]
+$footer[1;Chance of winning: 1/3]
+$setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins;$authorID];$message[1]]]
+$endif
+
+$cooldown[15s;Calm down you addict. %time%]
+
+$onlyIf[$message!=;You need to mention how much to bet!]
+$onlyIf[$isNumber[$message]!=false;Not a number.]
+$onlyIf[$checkContains[$message;-]!=true;No negative values allowed.]
+$onlyIf[$message<$getGlobalUserVar[Coins;$authorID];You don't have this much.]
+$onlyIf[$message>49;You need to bet at least $getVar[Coi]**50**.]
+$onlyIf[$message<9999;You can't gamble more than $getVar[Coi]9,999. Sorry.]
+`
+}
