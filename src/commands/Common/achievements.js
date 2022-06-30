a161bd59a24957b3726a39486be6b09e6dd9f1a4
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-06-30 09:25:36 +0100

    add useravatar to achievements.

--- src/commands/Common/achievements.js
+++ src/commands/Common/achievements.js
@@ -9,12 +9,14 @@ $onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has i
 $title[1;$username[$findUser[$message;yes]]'s achievements]
 $description[1;**__On Another Level__**[$getGlobalUserVar[ATKAchievement;$findUser[$message;yes]]]\nUpgrade to 2ATK.\nReward:  +<:RPGCoin:855767372534906920>150\n\n**__Your first time__** [$getGlobalUserVar[DailyAchievement;$findUser[$message;yes]]]\nCollect your daily reward for the first time.\nReward: +<:RPGCoin:855767372534906920>100\n\n**__The family farm__**[$getGlobalUserVar[FarmAchievement;$findUser[$message;yes]]]\nGo farming at least once.\nReward: +<:RPGCoin:855767372534906920>50\n\n
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
+$thumbnail[1;$userAvatar[$authorID]]
 
 $else
 $onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has interaction commands disabled from their settings.]
 $title[1;$username[$findUser[$message;yes]]'s achievements]
 $description[1;**__On Another Level__**[$getGlobalUserVar[ATKAchievement;$findUser[$message;yes]]]\nUpgrade to 2ATK.\nReward:  +<:RPGCoin:855767372534906920>150\n\n**__Your first time__** [$getGlobalUserVar[DailyAchievement;$findUser[$message;yes]]]\nCollect your daily reward for the first time.\nReward: +<:RPGCoin:855767372534906920>100\n\n**__The family farm__**[$getGlobalUserVar[FarmAchievement;$findUser[$message;yes]]]\nGo farming at least once.\nReward: +<:RPGCoin:855767372534906920>50\n\n
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
+$thumbnail[1;$userAvatar[$authorID]]
 $endif
 $globalCooldown[15s;Please wait before running this command again. This helps us enforce ratelimits. %time%]
 $suppressErrors
