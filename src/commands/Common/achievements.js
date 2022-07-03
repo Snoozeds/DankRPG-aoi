adf48a3f7c4f3c69fc32b933219f1455a27525ce
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-03 22:20:46 +0100

    fix achievements av.

--- src/commands/Common/achievements.js
+++ src/commands/Common/achievements.js
@@ -6,7 +6,7 @@ $onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has i
 $title[1;$username[$findUser[$message;yes]]'s achievements]
 $description[1;**__On Another Level__**[$getGlobalUserVar[ATKAchievement;$findUser[$message;yes]]]\nUpgrade to 2ATK.\nReward:  +<:RPGCoin:855767372534906920>150\n\n**__Your first time__** [$getGlobalUserVar[DailyAchievement;$findUser[$message;yes]]]\nCollect your daily reward for the first time.\nReward: +<:RPGCoin:855767372534906920>100\n\n**__The family farm__**[$getGlobalUserVar[FarmAchievement;$findUser[$message;yes]]]\nGo farming at least once.\nReward: +<:RPGCoin:855767372534906920>50\n\n**__Slots Addict__**[$getGlobalUserVar[slotAchievement;$findUser[$message;yes]]]\nSpent a total of $getVar[Coi]1000 on \`$getServerVar[Prefix;$guildID]slots\` (Current: $getVar[Coi]$getGlobalUserVar[slotAmount;$findUser[$message;yes]])\nReward: +<:RPGCoin:855767372534906920>100\n\n]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$thumbnail[1;$userAvatar[$authorID]]
+$thumbnail[1;$userAvatar[$findUser[$message;yes]]]
 $globalCooldown[5s;Please wait before running this command again. This helps us enforce ratelimits. %time%]
 $suppressErrors
 $onlyIf[$isBot[$authorID]!=true;]
