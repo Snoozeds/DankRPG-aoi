cc8aecede82581934018e502dd03bf940d192961
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-03 13:21:53 +0100

    Fix removecoins.

--- src/commands/Dev/removecoins.js
+++ src/commands/Dev/removecoins.js
@@ -3,7 +3,7 @@ name: "dev removecoins",
 aliases: ["dev remove coins", "devremovecoins", "devremove coins", "removecoins", "remove coins"],
 code: `
 $setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins;$findUser[$message[1];no]];$message[2]];$findUser[$message[1];no]]
-Gave $message[2] coins to <@$findUser[$message[1];no]>.
+Removed $message[2] coins from <@$findUser[$message[1];no]>.
 $onlyIf[$userExists[$findUser[$message[1];no]]!=false;<:alert:977143489697558569> The user you are trying to remove coins from doesn't exist!\n\`$getServerVar[Prefix;$guildID]dev removecoins <user> <amount>\`]
 $onlyIf[$isNumber[$message[2]]!=false;<:alert:977143489697558569> The second parameter for removecoins is invalid! Must be a number.\n\`$getServerVar[Prefix;$guildID]dev removecoins <user> <amount>\`]
 $onlyForIDs[$getVar[ownid];]
