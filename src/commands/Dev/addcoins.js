33ac483c3e481c52de305873e09a721b715bdde6
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-03 13:15:39 +0100

    Change addcoins.

--- src/commands/Dev/addcoins.js
+++ src/commands/Dev/addcoins.js
@@ -2,8 +2,9 @@ module.exports = {
 name: "dev addcoins",
 aliases: ["dev add coins", "devaddcoins", "devadd coins", "addcoins", "add coins"],
 code: `
-$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$findUser[$message[1];yes]];$message[2]];$findUser[$message[1];yes]]
-$onlyIf[$userExists[$findUser[$message[1];yes]]!=false;<:alert:977143489697558569> The user you are trying to add coins to doesn't exist!\n\`$getServerVar[Prefix;$guildID]dev addcoins <user> <amount>\`]
+$setGlobalUserVar[Coins;$sum[$getGlobalUserVar[Coins;$findUser[$message[1];no]];$message[2]];$findUser[$message[1];no]]
+Gave $message[2] coins to <@$findUser[$message[1];no]>.
+$onlyIf[$userExists[$findUser[$message[1];no]]!=false;<:alert:977143489697558569> The user you are trying to add coins to doesn't exist!\n\`$getServerVar[Prefix;$guildID]dev addcoins <user> <amount>\`]
 $onlyIf[$isNumber[$message[2]]!=false;<:alert:977143489697558569> The second parameter for addcoins is invalid! Must be a number.\n\`$getServerVar[Prefix;$guildID]dev addcoins <user> <amount>\`]
 $onlyForIDs[$getVar[ownid];]
 `
