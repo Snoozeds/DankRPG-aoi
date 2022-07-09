a74c8b2e3afb5a839dceff914d08cf5c44ebddec
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-09 15:13:29 +0100

    Add null check to banner.

--- src/commands/Common/banner.js
+++ src/commands/Common/banner.js
@@ -1,11 +1,19 @@
 module.exports = {
 name: "banner",
+$if: "v4",
 code: `
+
+$if[$authorBanner==null]
+You do not have a banner.
+$globalCooldown[2s;]
+
+$else
 $globalCooldown[5s;Please wait before using this command again. This helps us enforce ratelimits. %time%]
 $title[1;$username[$authorID]'s Banner]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $image[1;$authorBanner]
 $description[1;Download: [Link]($authorBanner)]
 $footer[1;Requested by $username[$authorID]]
-$addTimestamp[1;]`
+$addTimestamp[1;]
+$endif`
 }
