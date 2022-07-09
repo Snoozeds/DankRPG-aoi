5390a03f5babd66261987dd75adb141780677386
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-09 14:58:54 +0100

    Fix avatar link for mentioned users.

--- src/commands/Interactions/avatar.js
+++ src/commands/Interactions/avatar.js
@@ -6,7 +6,7 @@ $globalCooldown[5s;Please wait before using this command again. This helps us en
 $title[1;$username[$findUser[$message;yes]]'s Avatar]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $image[1;$userAvatar[$findUser[$message;yes]]]
-$description[1;Download: [Link]($userAvatar)]
+$description[1;Download: [Link]($userAvatar[$findUser[$message;yes]])]
 $footer[1;Requested by $username[$authorID]]
 $addTimestamp[1;]
 $onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has user interactions disabled from their settings.]
