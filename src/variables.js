ed62894ec1ee48bafd623c4cf553949ee74ad296
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-11 16:34:30 +0100

    Variables are now in a seperate file. No more restarts!!!

---
+++ src/variables.js
@@ -0,0 +1,61 @@
+module.exports = {
+  ownid: "764564962815115267", // Change this to your own ID. This is for when $botOwnerID won't work with multiple IDs (like in an $if statement). Can ONLY BE ONE ID.
+  Coi: "<:RPGCoin:855767372534906920>", // Variable to make typing the coin emoji easier.
+  Heart: "<:RPGHeart:855755205102534686>", // Variable to make typing the hp emoji easier.
+  Coins: "0",
+  HP: "100",
+  MaxHP : "100",
+  ATK: "1",
+  Prefix: "d!",
+  EmbedColor : "#FFE302",
+  FamilyMode : "False",
+  UserInt : "True",
+  FirstDaily: "True",
+  DailyAchievement: "<:Locked:899050875916541963>",
+  FarmO: "<:Orange:861639407978020914>",
+  FarmK: "<:Kiwi:861639983268626442>",
+  FarmS: "<:Strawberry:861641050930282516>",
+  FirstFarm: "True",
+  FarmAchievement: "<:Locked:899050875916541963>",
+  FirstATK: "True",
+  ATKAchievement: "<:Locked:899050875916541963>",
+  InBusiness: "False",
+  BusinessType: "None",
+  Stone : "0",
+  Wood : "0",
+  Slime: "0",
+  BoughtHouse: "False", // Bought house or not
+  BoughtPick: "False", // Bought pickaxe or not
+  BoughtBait: "False", // Bought bait or not
+  GiftL: "1", // Gift Stock
+  HouseL: "1", // House Stock
+  PickL: "1", // Pickaxe Stock
+  BaitL: "1", // Bait Stock
+  TopggToken: "", // No longer used
+  PetType: "none",
+  PetName: "Your pet hasn't been named yet. Run \`<prefix>petname <name>\`.",
+  HasPet: "false",
+  PetIMG : "",
+  Gold: "0",
+  Diamond: "0",
+  SlimeEmoji: "<:Slime:932047284814884996>",
+  StoneEmoji: "<:Stone:985624123868840036>",
+  GoldEmoji: "<:Gold:985624122333737041>",
+  DiamondEmoji: "<:Diamond:985624120844779600>",
+  AcceptEmoji: "<:Unlocked:899050875719393281>",
+  DenyEmoji: "<:Locked:899050875916541963>",
+  Sender: "",
+  MarriageDate: "", // Time in unix since user was married.
+  InRelationship: "False",
+  MarriedTo: "",
+  CatNotificationsOn: "True",
+  MiningTime: "10m",
+  deerStock: "0",
+  mooseStock: "0",
+  rabbitStock: "0",
+  foxStock: "0",
+  bjActive: "False",
+  bjBet: "",
+  slotAchievement: "<:Locked:899050875916541963>",
+  slotAmount: "0",
+}
