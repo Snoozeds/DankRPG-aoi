3b1f758648a6c34ffe9c5402527d442af2619737
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-13 10:12:44 +0100

    Added funnier kills

--- src/commands/Interactions/kill.js
+++ src/commands/Interactions/kill.js
@@ -1,13 +1,13 @@
 module.exports = {
 name: "kill",
 code: `
-$username kills $username[$findUser[$message;no]] by **$randomText[yeeting them;defenestrating them;ejecting them;stabbing them;kicking them;bonking them to death]**
+$username dies $username[$findUser[$message;no]] by **$randomText[being defenstrated;being bonked;being hit with a suspiciously large spoon;bowing too much and falling off a cliff;following the latest 100% safe trend;by pressing a suspiciously out of place red button]**.
 $onlyIf[$getGlobalUserVar[FamilyMode;$findUser[$message;no]]!=True;User has family mode.]
-$onlyIf[$findUser[$message;no]!=$clientID;haha I am immortal\nL]
+$onlyIf[$findUser[$message;no]!=$clientID;hmmm no, that's not possible.]
 $onlyIf[$findUser[$message;no]!=$authorID;I can't allow you to do that to yourself.]
 $onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has interactions turned off.]
 $globalCooldown[5s;Please wait before using this social command again. This helps us enforce ratelimits. %time%]
 $suppressErrors[Please mention a user.]
 $onlyIf[$isBot[$authorID]!=true;]
-	`
+`
 }
