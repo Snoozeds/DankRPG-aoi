9d0899610f39df80724c2ddf16a088dff0926c95
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-13 19:32:44 +0100

    you can now mention other users in banner.

--- src/commands/Common/banner.js
+++ src/commands/Common/banner.js
@@ -3,16 +3,16 @@ name: "banner",
 $if: "v4",
 code: `
 
-$if[$authorBanner==null]
-You do not have a banner.
+$if[$getUserBanner[$findUser[$message;yes]]==null]
+$username[$findUser[$message;yes]] does not have a custom banner. 
 $globalCooldown[2s;]
 
 $else
 $globalCooldown[5s;Please wait before using this command again. This helps us enforce ratelimits. %time%]
-$title[1;$username[$authorID]'s Banner]
+$title[1;$username[$findUser[$message;yes]]'s Banner]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$image[1;$authorBanner]
-$description[1;Download: [Link]($authorBanner)]
+$image[1;$getUserBanner[$findUser[$message;yes]]]
+$description[1;Download: [Link]($getUserBanner[$findUser[$message;yes]])]
 $footer[1;Requested by $username[$authorID]]
 $addTimestamp[1;]
 $endif`
