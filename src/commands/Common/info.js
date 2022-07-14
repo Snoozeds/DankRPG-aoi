c3d319bcdfccc39859e11b0c7629a20c817283aa
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-14 17:43:10 +0100

    Update info.js

--- src/commands/Common/info.js
+++ src/commands/Common/info.js
@@ -13,6 +13,6 @@ Member Count: $allMembersCount
 Node: $nodeVersion
 Aoi.JS: v$packageVersion]
 $color[1;$getGlobalUserVar[EmbedColor]]
-$footer[1;Bot by Snoozeds#0802 <3 | Build: v22]
+$footer[1;Bot by Snoozeds#0802 <3 | Build: v23]
 $globalCooldown[10s;Please wait before running this command again. This helps us enforce ratelimits. %time%]`
 } // Remove that footer if you'd like. Just credit me somewhere.
