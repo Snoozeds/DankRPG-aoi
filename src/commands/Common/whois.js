991bba87a1dfc12ec8c3106dac769375190d1739
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-15 09:11:57 +0100

    fix layout on whois/userinfo (mobile)

--- src/commands/Common/whois.js
+++ src/commands/Common/whois.js
@@ -3,10 +3,7 @@ name: "whois",
 aliases: ["wi", "ui", "userinfo"],
 code: `
 $description[1;
-\`$username[$findUser[$message;yes]]#$discriminator[$findUser[$message;yes]]\`
-**Highest Role:** <@&$highestRole[$findUser[$message;yes]]>
-**Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
-**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
+\`$username[$findUser[$message;yes]]#$discriminator[$findUser[$message;yes]]\`\n**Highest Role:** <@&$highestRole[$findUser[$message;yes]]>\n**Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>\n**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
 ]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $thumbnail[1;$userAvatar[$findUser[$message;yes]]]
--- src/commands/Slash/userinfo_slash.js
+++ src/commands/Slash/userinfo_slash.js
@@ -6,15 +6,15 @@ code: `
 $interactionReply[
 ;    
 {newEmbed:
-    {description:
-    \`$userTag[$interactionData[options.data[0].value]]\`
-    **Highest Role:** <@&$highestRole[$interactionData[options.data[0].value]]>
-    **Joined Discord:** <t:$truncate[$divide[$creationDate[$interactionData[options.data[0].value];$guildID;ms];1000]]:R>
-    **Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$interactionData[options.data[0].value];$guildID;ms];1000]]:R>
-    }
-    {image:$replaceText[$getUserBanner[$interactionData[options.data[0].value]];null;;1]}
-    {thumbnail:$userAvatar[$interactionData[options.data[0].value]]}
-    {color:$getGlobalUserVar[EmbedColor;$authorID]}
+{description:
+\`$userTag[$interactionData[options.data[0].value]]\`
+**Highest Role:** <@&$highestRole[$interactionData[options.data[0].value]]>
+**Joined Discord:** <t:$truncate[$divide[$creationDate[$interactionData[options.data[0].value];$guildID;ms];1000]]:R>
+**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$interactionData[options.data[0].value];$guildID;ms];1000]]:R>
+}
+{image:$replaceText[$getUserBanner[$interactionData[options.data[0].value]];null;;1]}
+{thumbnail:$userAvatar[$interactionData[options.data[0].value]]}
+{color:$getGlobalUserVar[EmbedColor;$authorID]}
 }
 ]
 `
