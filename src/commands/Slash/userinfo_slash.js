742902b0ba16d6a7c12f33bfa225f98d4c6f273b
SnoozedsGithub <88577069+SnoozedsGithub@users.noreply.github.com>
2022-07-25 00:01:02 +0100

    Update /userinfo to be the same as d!userinfo

--- src/commands/Slash/userinfo_slash.js
+++ src/commands/Slash/userinfo_slash.js
@@ -7,10 +7,12 @@ $interactionReply[
 ;    
 {newEmbed:
 {description:
-\`$userTag[$interactionData[options.data[0].value]]\`
+\`$userTag[$interactionData[options.data[0].value]] ($findUser[$interactionData[options.data[0].value]])\`
 **Highest Role:** <@&$highestRole[$interactionData[options.data[0].value]]>
 **Joined Discord:** <t:$truncate[$divide[$creationDate[$interactionData[options.data[0].value];$guildID;ms];1000]]:R>
 **Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$interactionData[options.data[0].value];$guildID;ms];1000]]:R>
+
+Links: [Avatar]($userAvatar[$interactionData[options.data[0].value]]) | [Banner]($replaceText[$getUserBanner[$interactionData[options.data[0].value]];null;;1])
 }
 {image:$replaceText[$getUserBanner[$interactionData[options.data[0].value]];null;;1]}
 {thumbnail:$userAvatar[$interactionData[options.data[0].value]]}
