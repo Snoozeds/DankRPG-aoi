0d2b10ff5b1e792e9b41940271a9dd32a9524d83
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-08-07 11:22:18 +0100

    Add MaxHP to userprofile

--- src/commands/Common/userprofile.js
+++ src/commands/Common/userprofile.js
@@ -3,17 +3,15 @@ name : "p",
 aliases : ["profile", "user", "stats", "bal"],
 code: `
 $onlyIf[$isBot[$findUser[$message;yes]]!=true;You cannot check a bot's profile.]
-$onlyIf[$getGlobalUserVar[UserInt;$findUser[$message;no]]!=False;This user has interaction commands disabled from their settings.]
 $title[1;$username[$findUser[$message;yes]]'s profile]
 $description[1;
 <:RPGCoin:855767372534906920>**$getGlobalUserVar[Coins;$findUser[$message;yes]]** \`Coins\`
-<:RPGHeart:855755205102534686>**$getGlobalUserVar[HP;$findUser[$message;yes]]** \`HP\`
+<:RPGHeart:855755205102534686>**$getGlobalUserVar[HP;$findUser[$message;yes]]/$getGlobalUserVar[MaxHP;$findUser[$message;yes]]** \`HP\`
 <:ATKF:915551150093250592>**$getGlobalUserVar[ATK;$findUser[$message;yes]]** \`ATK\`
 ]
 $thumbnail[1;$userAvatar[$findUser[$message;yes]]]
 $image[1;$replaceText[$getUserBanner[$findUser[$message;yes];1024];null;;1]]
 $color[1;$getGlobalUserVar[EmbedColor]]
 $globalCooldown[5s;Please wait before running this command again. This helps us enforce ratelimits. %time%]
-$onlyIf[$isBot[$authorID]!=true;]
 `
 }
