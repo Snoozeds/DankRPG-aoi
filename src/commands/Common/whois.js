6ac7c2b9d3c3a1de7fda9e745bb7690a0c308178
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-08-14 18:43:11 +0100

    Update whois.js

--- src/commands/Common/whois.js
+++ src/commands/Common/whois.js
@@ -15,7 +15,7 @@ $image[1;$replaceText[$getUserBanner[$findUser[$message;yes];1024];null;;1]]
 $else
 $description[1;
 \`$userTag[$findUser[$message;yes]] ($findUser[$message;yes])\`\n**Highest Role:** <@&$highestRole[$findUser[$message;yes]]>\n**Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>\n**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
-\nLink: [Avatar]($userAvatar[$findUser[$message;yes]])
+\nLinks: [Avatar]($userAvatar[$findUser[$message;yes]])
 ]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $thumbnail[1;$userAvatar[$findUser[$message;yes]]]
