f4cb317642bf11759ddd4271d8035bb34712b9ab
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-08-17 03:36:49 +0100

    ACTUALLY fix pinging as a prefix ;)

--- src/commands/Common/noprefix.js
+++ src/commands/Common/noprefix.js
@@ -3,11 +3,9 @@ name: "<@855479925863481345>",
 nonPrefixed: "true",
 $if: "v4",
 code: `
-$if[$commandInfo[$message;name]==]
 Hello! my prefix for this server is: \`$getServerVar[Prefix;$guildID]\`
 You can also ping me instead of using a prefix.
 $globalCooldown[5s;]
-$else
-$endif
+$argsCheck[<=1;]
 `
 }
