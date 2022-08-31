b79c5cb491304ec9d482d0f77b576f58ba2b11eb
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-08-31 22:09:34 +0100

    Replace if statement with replaceText (serverinfo)

--- src/commands/Common/serverinfo.js
+++ src/commands/Common/serverinfo.js
@@ -3,7 +3,6 @@ name: "serverinfo",
 aliases: "si",
 $if: "v4",
 code: `
-$if[$serverBanner==]
 $description[1;
 \`$serverName ($guildID)\`
 **Server Owner:** <@$ownerID>
@@ -11,25 +10,9 @@ $description[1;
 **Member Count:** $membersCount members
 **Boost Level:** $replaceText[$replaceText[$serverBoostLevel[$guildID];NONE;0;1];TIER_;;1]
 
-Links: [Icon]($serverIcon[$guildID])
+Links: [Icon]($serverIcon[$guildID])$replaceText[$replaceText[$checkCondition[$serverBanner!=];true; | [Banner]($serverBanner[$guildID]);1];false;;1]
 ]
 $thumbnail[1;$serverIcon[$guildID]]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-
-$else
-$description[1;
-\`$serverName ($guildID)\`
-**Server Owner:** <@$ownerID>
-**Server Created:** <t:$truncate[$divide[$creationDate[$guildID;ms];1000]]:R>
-**Member Count:** $membersCount members
-**Boost Level:** $replaceText[$replaceText[$serverBoostLevel[$guildID];NONE;0;1];TIER_;;1]
-
-Links: [Icon]($serverIcon[$guildID]) | [Banner]($serverBanner[$guildID]) 
-]
-$thumbnail[1;$serverIcon[$guildID]]
-$image[$serverBanner[$guildID;2048]]
-$color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$endif
-
 $globalCooldown[10s;Please wait before running this command again. This helps us enforce ratelimits. %time%]`
 }
