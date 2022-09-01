bb68e73d65bcb50f7fd7a49afcb1736f09b8516c
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-01 02:05:56 +0100

    Add prefix character limit (<8).

--- src/commands/Settings/prefix.js
+++ src/commands/Settings/prefix.js
@@ -7,5 +7,6 @@ $suppressErrors
 $onlyPerms[$getServerVar[Permissions;$guildID];This server's prefix is $getServerVar[Prefix;$guildID]\n(You can't change this because you don't have the  \`$getServerVar[Permissions;$guildID]\` permission)]
 $onlyIf[$isBot[$authorID]!=true;]
 $onlyIf[$message!=;You need to provide a proper prefix.]
+$onlyIf[$charCount[$message]<8;\`$message\` is longer than 8 characters. Please try to keep your prefix short. Thanks]
 `
 }
