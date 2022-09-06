479d2dfe88237c54f67147874901baaa59075ca4
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-06 11:22:29 +0100

    Cancel plans for /buy.

--- src/commands/Slash/shop_commands_slash.js
+++ src/commands/Slash/shop_commands_slash.js
@@ -8,10 +8,11 @@ $interactionReply[;
 {newEmbed:
 {title:Welcome to my shop!}
 {description:Here are my items on sale.\n
-<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your HP by 100. | $getVar[Coi]**750**\n/buy hp \n
-$if[$getGlobalUserVar[PickL;$authorID]==1] \n<:pickaxe:963846459126583347> **__Pickaxe__** \nDecreases your mining speed by 5m. | **100 Stone** \n**In stock: $getGlobalUserVar[PickL;$authorID]** \n/buy pick \n\n $endif $if[$getGlobalUserVar[BaitL;$authorID]==1] \n<:Bait:916369932072128544> **__Bait__** \nFind double the animals in \`$getServerVar[Prefix]hunt\`. | **100 Slime** \n**In stock: $getGlobalUserVar[BaitL;$authorID]** \n/buy bait \n\n $endif
+<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your HP by 100. | $getVar[Coi]**750**\n$getServerVar[Prefix;$guildID]buyhp \n
+$if[$getGlobalUserVar[PickL;$authorID]==1] \n<:pickaxe:963846459126583347> **__Pickaxe__** \nDecreases your mining speed by 5m. | **100 Stone** \n**In stock: $getGlobalUserVar[PickL;$authorID]** \n$getServerVar[Prefix;$guildID]buypick \n\n $endif $if[$getGlobalUserVar[BaitL;$authorID]==1] \n<:Bait:916369932072128544> **__Bait__** \nFind double the animals in \`$getServerVar[Prefix]hunt\`. | **100 Slime** \n**In stock: $getGlobalUserVar[BaitL;$authorID]** \n$getServerVar[Prefix;$guildID]buybait \n\n $endif
 Other Shop commands:\n\`upgrade\`, \`hp\`, \`heal\`, \`petshop\`}
 {color:$getGlobalUserVar[EmbedColor]}}
 ]
 `
-}]
+}
+]
