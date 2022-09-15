113b3900c5e790d1a3e14bdaad385eb406ce1e34
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-15 20:30:58 +0100

    Add an error message to repeat when there's no message.

--- src/commands/Common/repeat.js
+++ src/commands/Common/repeat.js
@@ -6,5 +6,6 @@ $description[1;\`$message\`]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $addTimestamp[1]
 $globalCooldown[15s;I will not spam your messages. %time%]
+$onlyIf[$message!=;{newEmbed:{description: You need to provide a message to repeat.\n\`$getServerVar[Prefix;$guildID]repeat <message>\`}{color: #ff2050}}]
 $onlyIf[$isBot[$authorID]!=true;]`
 }
