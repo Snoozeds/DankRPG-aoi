3b9ba3d7ba62e199451243536a6fb4dab32065d0
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-16 12:27:24 +0100

    Add additional errors to addcoins and removecoins

--- src/commands/Admin/removecoins.js
+++ src/commands/Admin/removecoins.js
@@ -6,6 +6,7 @@ $setGlobalUserVar[Coins;$sub[$getGlobalUserVar[Coins;$findUser[$message[1];no]];
 Removed $getVar[Coi]$message[2] from **$userTag[$findUser[$message[1];no]]**.
 $onlyIf[$userExists[$findUser[$message[1];no]]!=false;<:alert:977143489697558569> The user you are trying to remove coins from doesn't exist!\n\`$getServerVar[Prefix;$guildID]removecoins <user> <amount>\`]
 $onlyIf[$isNumber[$message[2]]!=false;<:alert:977143489697558569> The second parameter for removecoins is invalid! Must be a number.\n\`$getServerVar[Prefix;$guildID]removecoins <user> <amount>\`]
+$onlyIf[$message!=;<:alert:977143489697558569> You are missing parameters!\n\`$getServerVar[Prefix;$guildID]removecoins <user> <amount>\`]
 $onlyPerms[$getServerVar[Permissions;$guildID];Sorry, you cannot use this command as you don't have the \`$getServerVar[Permissions;$guildID]\` permission.]
 `
 } 
