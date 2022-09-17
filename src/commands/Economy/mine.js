a440c853fc32f3aa1100cabf2517bec2ca4f0f89
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-17 23:20:41 +0100

    Fix gaining more diamonds and decrease cooldown.

--- src/commands/Economy/mine.js
+++ src/commands/Economy/mine.js
@@ -6,7 +6,7 @@ code: `
 $if[$getGlobalUserVar[MiningTime;$authorID]==10m]
 $channelSendMessage[$channelID;<@$authorID>\nYou mined:\n$getVar[StoneEmoji]**$random[10;20]** Stone\n$getVar[GoldEmoji]**$random[5;10]** Gold\n$getVar[DiamondEmoji]**$random[2;3]** Diamond(s)]
 $setGlobalUserVar[Gold;$sum[$getGlobalUserVar[Gold;$authorID];$random[5;10]];$authorID]
-$setGlobalUserVar[Diamond;$sum[$getGlobalUserVar[Diamond;$authorID];$random[1;5]];$authorID]
+$setGlobalUserVar[Diamond;$sum[$getGlobalUserVar[Diamond;$authorID];$random[2;3]];$authorID]
 $setGlobalUserVar[Stone;$sum[$getGlobalUserVar[Stone;$authorID];$random[10;20]];$authorID]
 $wait[10m]
 $channelSendMessage[$channelID;You have started mining. Come back in $getGlobalUserVar[MiningTime;$authorID] to see what you have mined.]
@@ -22,7 +22,7 @@ $channelSendMessage[$channelID;You have started mining. Come back in $getGlobalU
 $endif
 $endif
 
-$globalCooldown[$sum[$replaceText[$getGlobalUserVar[MiningTime;$authorID];m; ;-1];5]m;
+$globalCooldown[$sum[$replaceText[$getGlobalUserVar[MiningTime;$authorID];m; ;-1];1]m;
 {newEmbed:
 {title:Slow down $username[$authorID]!}
 {description:You can use \`mine\` again in **%min%m %sec%s**.}
