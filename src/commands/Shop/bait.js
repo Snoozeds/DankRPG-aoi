1daaa344b54a7a41e51b366fe23652bd8d434667
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-22 00:56:42 +0100

    Updated some item descriptions in shop.

--- src/commands/Shop/bait.js
+++ src/commands/Shop/bait.js
@@ -5,7 +5,7 @@ code: `
 You bought bait for **100 Slime**
 $setGlobalUserVar[Slime;$sub[$getGlobalUserVar[Slime];100];$authorID]
 $setGlobalUserVar[BoughtBait;True;$authorID]
-$setGlobalUserVar[BaitL;0:$authorID]
+$setGlobalUserVar[BaitL;0;$authorID]
 $onlyIf[$getGlobalUserVar[Slime;$authorID]>99;You do not have enough **slime** to buy **bait**. You need at least **100 Slime**.]
 $onlyIf[$getGlobalUserVar[BoughtBait]!=True;You already have bait smh]`
 }
--- src/commands/Shop/shop.js
+++ src/commands/Shop/shop.js
@@ -4,8 +4,8 @@ $if: "v4",
 code: `
 $title[1;Welcome to my shop!]
 $description[1;Here are my items on sale.\n
-<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your HP by 100. | <:RPGCoin:855767372534906920>**750**\n$getServerVar[Prefix]buyhp \n
-$if[$getGlobalUserVar[PickL;$authorID]==1] \n<:pickaxe:963846459126583347> **__Pickaxe__** \nDecreases your mining speed by 5m. | **100 Stone** \n**In stock: $getGlobalUserVar[PickL;$authorID]** \n$getServerVar[Prefix]buypick \n\n $endif $if[$getGlobalUserVar[BaitL;$authorID]==1] \n<:Bait:916369932072128544> **__Bait__** \nFind double the animals in \`$getServerVar[Prefix]hunt\`. | **100 Slime** \n**In stock: $getGlobalUserVar[BaitL;$authorID]** \n$getServerVar[Prefix]buybait \n\n $endif
+<:hpDrop:915888646454927380> **__hpDrop__**\nIncreases your MaxHP by 100. | <:RPGCoin:855767372534906920>**750**\n$getServerVar[Prefix]buyhp \n
+$if[$getGlobalUserVar[PickL;$authorID]==1] \n<:pickaxe:963846459126583347> **__Pickaxe__** \nDecreases your mining speed by 5m. | **100 Stone** \n**In stock: $getGlobalUserVar[PickL;$authorID]** \n$getServerVar[Prefix]buypick \n\n $endif $if[$getGlobalUserVar[BaitL;$authorID]==0] \n<:Bait:916369932072128544> **__Bait__** \nFind double the animals in \`$getServerVar[Prefix;$guildID]hunt\` and \`$getServerVar[Prefix;$guildID]pethunt\`. | **100 Slime** \n**In stock: $getGlobalUserVar[BaitL;$authorID]** \n$getServerVar[Prefix]buybait \n\n $endif
 Other Shop commands:\n\`megashop\`,\`upgrade\`, \`hp\`, \`heal\`, \`petshop\`]
 $color[1;$getGlobalUserVar[EmbedColor]]`
 }
