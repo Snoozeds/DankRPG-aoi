67149ac73d00846fd0b679f764623b73b9565f41
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-22 03:49:02 +0100

    Decrease response times for profile & inventory!

--- src/commands/Common/inventory.js
+++ src/commands/Common/inventory.js
@@ -1,19 +1,20 @@
 module.exports = {
 name: "inventory",
 aliases: ["inv", "i", "in"],
-code: `$title[1;$username[$findUser[$message;yes]]'s Inventory]
+code: `$title[1;$username[$get[u]]'s Inventory]
 $description[1;
-$if[$getGlobalUserVar[Wood;$findUser[$message;yes]]!=0;**:wood: Wood:** $getGlobalUserVar[Wood;$findUser[$message;yes]]] $if[$getGlobalUserVar[Slime;$findUser[$message;yes]]!=0;\n**$getVar[SlimeEmoji] Slime:** $getGlobalUserVar[Slime;$findUser[$message;yes]]] $if[$getGlobalUserVar[Stone;$findUser[$message;yes]]!=0;\n**$getVar[StoneEmoji] Stone:** $getGlobalUserVar[Stone;$findUser[$message;yes]]] $if[$getGlobalUserVar[Gold;$findUser[$message;yes]]!=0;\n**$getVar[GoldEmoji] Gold:** $getGlobalUserVar[Gold;$findUser[$message;yes]]] $if[$getGlobalUserVar[Diamond;$findUser[$message;yes]]!=0;\n**$getVar[DiamondEmoji] Diamonds:** $getGlobalUserVar[Diamond;$findUser[$message;yes]]] $if[$getGlobalUserVar[HPPotion;$findUser[$message;yes]]!=0;\n**<:hppotion:1022298361702920353> HP Potions:** $getGlobalUserVar[HPPotion;$findUser[$message;yes]]] $if[$getGlobalUserVar[foxStock;$findUser[$message;yes]]!=0;\n**Foxes:** $getGlobalUserVar[foxStock;$findUser[$message;yes]]] $if[$getGlobalUserVar[deerStock;$findUser[$message;yes]]!=0;\n**Deer:** $getGlobalUserVar[deerStock;$findUser[$message;yes]]] $if[$getGlobalUserVar[mooseStock;$findUser[$message;yes]]!=0;\n**Moose:** $getGlobalUserVar[mooseStock;$findUser[$message;yes]]] $if[$getGlobalUserVar[rabbitStock;$findUser[$message;yes]]!=0;\n**Rabbits:** $getGlobalUserVar[rabbitStock;$findUser[$message;yes]]]
+$if[$getGlobalUserVar[Wood;$get[u]]!=0;**:wood: Wood:** $getGlobalUserVar[Wood;$get[u]]] $if[$getGlobalUserVar[Slime;$get[u]]!=0;\n**$getVar[SlimeEmoji] Slime:** $getGlobalUserVar[Slime;$get[u]]] $if[$getGlobalUserVar[Stone;$get[u]]!=0;\n**$getVar[StoneEmoji] Stone:** $getGlobalUserVar[Stone;$get[u]]] $if[$getGlobalUserVar[Gold;$get[u]]!=0;\n**$getVar[GoldEmoji] Gold:** $getGlobalUserVar[Gold;$get[u]]] $if[$getGlobalUserVar[Diamond;$get[u]]!=0;\n**$getVar[DiamondEmoji] Diamonds:** $getGlobalUserVar[Diamond;$get[u]]] $if[$getGlobalUserVar[HPPotion;$get[u]]!=0;\n**<:hppotion:1022298361702920353> HP Potions:** $getGlobalUserVar[HPPotion;$get[u]]] $if[$getGlobalUserVar[foxStock;$get[u]]!=0;\n**Foxes:** $getGlobalUserVar[foxStock;$get[u]]] $if[$getGlobalUserVar[deerStock;$get[u]]!=0;\n**Deer:** $getGlobalUserVar[deerStock;$get[u]]] $if[$getGlobalUserVar[mooseStock;$get[u]]!=0;\n**Moose:** $getGlobalUserVar[mooseStock;$get[u]]] $if[$getGlobalUserVar[rabbitStock;$get[u]]!=0;\n**Rabbits:** $getGlobalUserVar[rabbitStock;$get[u]]]
 
-Total inventory value: $getVar[Coi]$sum[$multi[$getGlobalUserVar[Stone;$findUser[$message;yes]];3];$multi[$getGlobalUserVar[Gold;$findUser[$message;yes]];20];$multi[$getGlobalUserVar[Diamond;$findUser[$message;yes]];100];$multi[$getGlobalUserVar[foxStock;$findUser[$message;yes]];50];$multi[$getGlobalUserVar[deerStock;$findUser[$message;yes]];75];$multi[$getGlobalUserVar[mooseStock;$findUser[$message;yes]];100];$multi[$getGlobalUserVar[rabbitStock;$findUser[$message;yes]];125];$multi[$getGlobalUserVar[Wood;$findUser[$message;yes]];1];$multi[$getGlobalUserVar[Slime;$findUser[$message;yes]];2]]]
+Total inventory value: $getVar[Coi]$sum[$multi[$getGlobalUserVar[Stone;$get[u]];3];$multi[$getGlobalUserVar[Gold;$get[u]];20];$multi[$getGlobalUserVar[Diamond;$get[u]];100];$multi[$getGlobalUserVar[foxStock;$get[u]];50];$multi[$getGlobalUserVar[deerStock;$get[u]];75];$multi[$getGlobalUserVar[mooseStock;$get[u]];100];$multi[$getGlobalUserVar[rabbitStock;$get[u]];125];$multi[$getGlobalUserVar[Wood;$get[u]];1];$multi[$getGlobalUserVar[Slime;$get[u]];2]]]
 $color[1;$getGlobalUserVar[EmbedColor]]
-$thumbnail[$userAvatar[$findUser[$message;yes]]]
-$botTyping
+$thumbnail[$userAvatar[$get[u]]]
 
 $globalCooldown[10s;
 {newEmbed:
 {title:Slow down $username[$authorID]!}
 {description:You can use \`inventory\` again in **%sec%s**.}
 {color:#ff2050}}]
+
+$let[u;$findUser[$message;yes]]
 `
 }
--- src/commands/Common/userprofile.js
+++ src/commands/Common/userprofile.js
@@ -2,16 +2,16 @@ module.exports = {
 name : "p",
 aliases : ["profile", "user", "stats", "bal"],
 code: `
-$onlyIf[$isBot[$findUser[$message;yes]]!=true;You cannot check a bot's profile.]
-$title[1;$username[$findUser[$message;yes]]'s profile]
+$onlyIf[$isBot[$get[u]]!=true;You cannot check a bot's profile.]
+$title[1;$username[$get[u]]'s profile]
 $description[1;
-$getVar[Coi]**$getGlobalUserVar[Coins;$findUser[$message;yes]]** \`Coins\`
-$getVar[MegaCoi]**$getGlobalUserVar[MegaCoins;$findUser[$message;yes]]** \`MegaCoins\`
-$getVar[Heart]$replaceText[$replaceText[$checkCondition[$getGlobalUserVar[HP;$findUser[$message;yes]]==$getGlobalUserVar[MaxHP;$findUser[$message;yes]]];true;**$getGlobalUserVar[HP;$findUser[$message;yes]]**;1];false;**$getGlobalUserVar[HP;$findUser[$message;yes]]/$getGlobalUserVar[MaxHP;$findUser[$message;yes]]**;1] \`HP\`
-<:ATKF:915551150093250592>**$getGlobalUserVar[ATK;$findUser[$message;yes]]** \`ATK\`
+$getVar[Coi]**$getGlobalUserVar[Coins;$get[u]]** \`Coins\`
+$getVar[MegaCoi]**$getGlobalUserVar[MegaCoins;$get[u]]** \`MegaCoins\`
+$getVar[Heart]$replaceText[$replaceText[$checkCondition[$getGlobalUserVar[HP;$get[u]]==$getGlobalUserVar[MaxHP;$get[u]]];true;**$getGlobalUserVar[HP;$get[u]]**;1];false;**$getGlobalUserVar[HP;$get[u]]/$getGlobalUserVar[MaxHP;$get[u]]**;1] \`HP\`
+<:ATKF:915551150093250592>**$getGlobalUserVar[ATK;$get[u]]** \`ATK\`
 ]
-$thumbnail[1;$userAvatar[$findUser[$message;yes]]]
-$image[1;$replaceText[$getUserBanner[$findUser[$message;yes];1024];null;;1]]
+$thumbnail[1;$userAvatar[$get[u]]]
+$image[1;$replaceText[$getUserBanner[$get[u];1024];null;;1]]
 $color[1;$getGlobalUserVar[EmbedColor]]
 
 $globalCooldown[5s;
@@ -19,5 +19,7 @@ $globalCooldown[5s;
 {title:Slow down $username[$authorID]!}
 {description:You can use \`profile\` again in **%sec%s**.}
 {color:#ff2050}}]
+
+$let[u;$findUser[$message;yes]]
 `
 }
