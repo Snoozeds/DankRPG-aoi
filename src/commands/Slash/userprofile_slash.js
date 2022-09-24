09f72d64dc921087316956d9ccae889d94b0ec6d
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-24 19:26:31 +0100

    Add megacoins to /profile!

--- src/commands/Slash/userprofile_slash.js
+++ src/commands/Slash/userprofile_slash.js
@@ -8,6 +8,7 @@ $interactionReply[
 {newEmbed:
 {title:$username[$interactionData[options.data[0].value]]'s profile}
 {description:<:RPGCoin:855767372534906920>**$getGlobalUserVar[Coins;$interactionData[options.data[0].value]]** \`Coins\`
+$getVar[MegaCoi]**$getGlobalUserVar[MegaCoins;$interactionData[options.data[0].value]]** \`MegaCoins\`
 <:RPGHeart:855755205102534686>$replaceText[$replaceText[$checkCondition[$getGlobalUserVar[HP;$interactionData[options.data[0].value]]==$getGlobalUserVar[MaxHP;$interactionData[options.data[0].value]]];true;**$getGlobalUserVar[HP;$interactionData[options.data[0].value]]**;1];false;**$getGlobalUserVar[HP;$interactionData[options.data[0].value]]/$getGlobalUserVar[MaxHP;$interactionData[options.data[0].value]]**;1] \`HP\`
 <:ATKF:915551150093250592>**$getGlobalUserVar[ATK;$interactionData[options.data[0].value]]** \`ATK\`}
 {thumbnail:$userAvatar[$interactionData[options.data[0].value]]}
