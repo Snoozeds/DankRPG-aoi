7e5a2ebb609fe5a8b53a249de5556fddbe0ef9a8
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-26 03:15:58 +0100

    Remove incorrect character from repeat.

--- src/commands/Common/repeat.js
+++ src/commands/Common/repeat.js
@@ -5,7 +5,7 @@ code: `$title[1;Repeated message from $username[$authorID]:]
 $description[1;\`$message\`]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $addTimestamp[1]
-$onlyIf[$message!=;{newEmbed:{description: You need to provide a message to repeat.\n\`$getServerVar[Prefix;$guildID]repeat <message>\`}{color: #ff2050}}]'
+$onlyIf[$message!=;{newEmbed:{description: You need to provide a message to repeat.\n\`$getServerVar[Prefix;$guildID]repeat <message>\`}{color: #ff2050}}]
 
 $globalCooldown[10s;
 {newEmbed:
