f838b6091086204e5f99f2c5c29c83c12869acbb
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-28 21:35:13 +0100

    fix $if check in refresh.

--- src/commands/Dev/updatecommands.js
+++ src/commands/Dev/updatecommands.js
@@ -1,8 +1,11 @@
 module.exports = {
 name: "update",
+$if: "v4",
 aliases: ["refresh", "refreshcommands", "updatecommands"],
-code: `$onlyForIDs[$getVar[ownid];]
+code: `
+$if[$authorID==$getVar[ownid]]
 $channelSendMessage[$channelID;Updated commands.]
 $updateCommands
-$onlyIf[$isBot[$authorID]!=true;]`
+$else
+$endif`
 } // This will NOT add new variables. It only refreshes each command file changed.
