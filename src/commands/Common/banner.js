02e38541c315255e3fb38185063fcc727a4c91b3
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2022-10-20 02:28:35 +0100

    Fix apostrophe in "banner" if username ends with s.

--- src/commands/Common/banner.js
+++ src/commands/Common/banner.js
@@ -8,7 +8,7 @@ $replaceText[$replaceText[$checkCondition[$get[u]==$authorID];true;You do not;1]
 $replaceText[$replaceText[$checkCondition[$get[u]==$authorID];true;Your;1];false;Their;1] banner color is **$getUserAccentColor[$get[u]]**.
 
 $else
-$title[1;$username[$get[u]]'s Banner]
+$title[1;$username[$get[u]]$replaceText[$replaceText[$checkCondition[$stringEndsWith[$toUpperCase[$username[$get[u]]];S]==true];true;';1];false;'s;1] Banner]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
 $image[1;$getUserBanner[$get[u]]]
 $description[1;Download: [Link]($getUserBanner[$get[u]])]
