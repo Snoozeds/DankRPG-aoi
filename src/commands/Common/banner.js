2dd059c26831afd179ee844eb36c7b966530ffe2
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2022-10-20 02:16:40 +0100

    Reduce response time in banner.

--- src/commands/Common/banner.js
+++ src/commands/Common/banner.js
@@ -4,14 +4,14 @@ $if: "v4",
 code: `
 
 $if[$getUserBanner[$findUser[$message;yes]]==null]
-$replaceText[$replaceText[$checkCondition[$findUser[$message;yes]==$authorID];true;You do not;1];false;**$username[$findUser[$message;yes]]** does not;1] have a custom nitro banner.
-$replaceText[$replaceText[$checkCondition[$findUser[$message;yes]==$authorID];true;Your;1];false;Their;1] banner color is **$getUserAccentColor[$findUser[$message;yes]]**.
+$replaceText[$replaceText[$checkCondition[$get[u]==$authorID];true;You do not;1];false;**$username[$get[u]]** does not;1] have a custom nitro banner.
+$replaceText[$replaceText[$checkCondition[$get[u]==$authorID];true;Your;1];false;Their;1] banner color is **$getUserAccentColor[$get[u]]**.
 
 $else
-$title[1;$username[$findUser[$message;yes]]'s Banner]
+$title[1;$username[$get[u]]'s Banner]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$image[1;$getUserBanner[$findUser[$message;yes]]]
-$description[1;Download: [Link]($getUserBanner[$findUser[$message;yes]])]
+$image[1;$getUserBanner[$get[u]]]
+$description[1;Download: [Link]($getUserBanner[$get[u]])]
 $footer[1;Requested by $username[$authorID]]
 $addTimestamp[1;]
 $endif
@@ -20,5 +20,7 @@ $globalCooldown[10s;
 {newEmbed:
 {title:Slow down $username[$authorID]!}
 {description:You can use \`banner\` again in **%sec%s**.}
-{color:#ff2050}}]`
-}
+{color:#ff2050}}]
+
+$let[u;$findUser[$message;yes]]`
+} // For some reason $getUserBanner doesn't work with $get. Maybe because the function starts with "$get"??
