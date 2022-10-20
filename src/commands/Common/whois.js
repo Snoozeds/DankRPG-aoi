5c7f361570a33ff4f62cfc59c2f8114017d368d0
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2022-10-20 02:41:55 +0100

    Reduce response time (userinfo)

--- src/commands/Common/whois.js
+++ src/commands/Common/whois.js
@@ -3,24 +3,26 @@ name: "whois",
 $if: "v4",
 aliases: ["wi", "ui", "userinfo"],
 code: `
-$if[$getUserBanner[$findUser[$message;yes]]!=null]
+$if[$getUserBanner[$get[u]]!=null]
 $description[1;
-\`$userTag[$findUser[$message;yes]] ($findUser[$message;yes])\`\n**Highest Role:** <@&$highestRole[$findUser[$message;yes]]>\n**Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>\n**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
-\nLinks: [Avatar]($userAvatar[$findUser[$message;yes]]) | [Banner]($getUserBanner[$findUser[$message;yes]])
+\`$userTag[$get[u]] ($get[u])\`\n**Highest Role:** <@&$highestRole[$get[u]]>\n**Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>\n**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
+\nLinks: [Avatar]($userAvatar[$get[u]]) | [Banner]($getUserBanner[$get[u]])
 ]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$thumbnail[1;$userAvatar[$findUser[$message;yes]]]
-$image[1;$replaceText[$getUserBanner[$findUser[$message;yes];1024];null;;1]]
+$thumbnail[1;$userAvatar[$get[u]]]
+$image[1;$replaceText[$getUserBanner[$get[u];1024];null;;1]]
 
 $else
 $description[1;
-\`$userTag[$findUser[$message;yes]] ($findUser[$message;yes])\`\n**Highest Role:** <@&$highestRole[$findUser[$message;yes]]>\n**Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>\n**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
-\nLinks: [Avatar]($userAvatar[$findUser[$message;yes]])
+\`$userTag[$get[u]] ($get[u])\`\n**Highest Role:** <@&$highestRole[$get[u]]>\n**Joined Discord:** <t:$truncate[$divide[$creationDate[$findUser[$message[1]];$guildID;ms];1000]]:R>\n**Joined Server:** <t:$truncate[$divide[$memberJoinedDate[$findUser[$message[1]];$guildID;ms];1000]]:R>
+\nLinks: [Avatar]($userAvatar[$get[u]])
 ]
 $color[1;$getGlobalUserVar[EmbedColor;$authorID]]
-$thumbnail[1;$userAvatar[$findUser[$message;yes]]]
-$image[1;$replaceText[$getUserBanner[$findUser[$message;yes];1024];null;;1]]
+$thumbnail[1;$userAvatar[$get[u]]]
+$image[1;$replaceText[$getUserBanner[$get[u];1024];null;;1]]
 $endif
+
+$let[u;$findUser[$message;yes]]
 
 $globalCooldown[10s;
 {newEmbed:
